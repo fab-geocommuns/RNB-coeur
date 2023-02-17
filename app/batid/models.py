36951edf72cd875ddc1ed7a22e0e488a036e665e
pauletienney <paul.etienney@beta.gouv.fr>
2023-02-17 17:16:16 +0100

    removed lineage model

--- app/batid/models.py
+++ app/batid/models.py
@@ -1,15 +1,12 @@
 from django.contrib.gis.db import models
 
 class Building(models.Model):
-    rnb_id = models.CharField(max_length=6, null=False)
+    rnb_id = models.CharField(max_length=6, null=False, unique=True, db_index=True)
     source = models.CharField(max_length=10, null=False)
     point = models.PointField(null=True)
+    parents = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='children')
 
 class ExtRef(models.Model):
-    building = models.ForeignKey(Building)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
     source = models.CharField(max_length=10, null=False, db_index=True)
     ext_id = models.CharField(max_length=50, null=False, db_index=True)
-
-class Lineage(models.Model):
-    parent = models.ForeignKey(Building)
-    child = models.ForeignKey(Building)
