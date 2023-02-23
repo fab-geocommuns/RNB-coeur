85587ed870bfa93754014aaf3a997ac1c545d531
pauletienney <paul.etienney@beta.gouv.fr>
2023-02-23 17:43:46 +0100

    paginate results in endpoint

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,24 +1,14 @@
 from pprint import pprint
 
 from django.core.management.base import BaseCommand
-from batid.logic.building import generate_id
+from batid.models import Building
 
 class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
 
-        ids = []
-        for i in range(23_000_000):
-
-            id = generate_id()
-            ids.append(id)
-
-
-        unique_ids = set(ids)
-
-        self.stdout.write(self.style.SUCCESS(f'Generated {len(ids)} ids'))
-        self.stdout.write(self.style.SUCCESS(f'Unique ids: {len(unique_ids)}'))
+        Building.objects.all().delete()
 
 
 
--- app/batid/views.py
+++ app/batid/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from batid.models import Building
 from batid.serializers import BuildingSerializer
+from rest_framework.pagination import PageNumberPagination
 
 class BuildingViewSet(viewsets.ModelViewSet):
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
+    http_method_names = ['get']
+    pagination_class = PageNumberPagination
