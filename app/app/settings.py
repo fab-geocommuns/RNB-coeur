f757d24624be6a274a2b051972f64e3553a05d06
pauletienney <paul.etienney@beta.gouv.fr>
2023-03-03 11:21:54 +0100

    prepare prod for proto

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 500
 }
 
 
