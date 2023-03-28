27bf4c5a14991fc49ad0bcd7ac5c1f33b686fb4d
pauletienney <paul.etienney@beta.gouv.fr>
2023-03-28 22:05:56 +0200

    perf on bdtopo read

--- worker/sandbox.py
+++ worker/sandbox.py
@@ -4,11 +4,14 @@ from shapely.geometry.polygon import Polygon
 import psycopg2
 import psycopg2.extras
 from jobs.import_bdnb7 import import_bdnb7
+from jobs.import_bdtopo import import_bdtopo
 from jobs.inspect_candidates import Inspector
 
 def sandbox():
 
-    pass
+    import_bdtopo('044')
+
+
 
 
 
