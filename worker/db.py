6f6b031385450b6a2ad0e1c47550bec920fec3ac
pauletienney <paul.etienney@beta.gouv.fr>
2023-03-28 16:06:39 +0200

    db host on worker was hard coded

--- worker/db.py
+++ worker/db.py
@@ -6,8 +6,9 @@ from shapely import wkb
 db_name = os.environ.get("SQL_NAME")
 db_user = os.environ.get("SQL_USER")
 db_password = os.environ.get("SQL_PASSWORD")
+db_host = os.environ.get("SQL_HOST")
 
-conn = psycopg2.connect(f"dbname='{db_name}' user='{db_user}' host='db' password='{db_password}'")
+conn = psycopg2.connect(f"dbname='{db_name}' user='{db_user}' host='{db_host}' password='{db_password}'")
 
 def dbgeom_to_shapely(rowgeom):
     return wkb.loads(rowgeom, hex=True)
