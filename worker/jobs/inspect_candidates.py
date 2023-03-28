70b912a970c1e892c10d90248d5985c98fbaf23a
pauletienney <paul.etienney@beta.gouv.fr>
2023-03-28 09:20:58 +0200

    performance for inspector

--- worker/jobs/inspect_candidates.py
+++ worker/jobs/inspect_candidates.py
@@ -1,52 +1,61 @@
-from db import conn, shapely_to_dbgeom
+import psycopg2
+from db import conn
 from psycopg2.extras import RealDictCursor
-from shapely import wkb
-from settings import settings
 from logic.candidate import row_to_candidate, Candidate
 from logic.building import generate_id
-
+from settings import settings
 
 class Inspector:
 
-    BATCH_SIZE = 10000
+    BATCH_SIZE = 50000
 
     def inspect(self) -> int:
 
-        # get some candidates not inspected yet
-        candidates = self.__get_candidates()
-
-        print(f"got {len(candidates)} candidates to inspect")
-
-        for c_row in candidates:
-            c = row_to_candidate(c_row)
-            self.__inspect(c)
-
-        return len(candidates)
-
-    def __get_candidates(self) -> list:
-
-        q = "SELECT * from batid_candidate where inspected_at is null order by created_at limit %(limit)s"
+        q, params = self.get_matches_query()
 
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(q, {'limit': self.BATCH_SIZE})
-            return cur.fetchall()
+            cur.execute(q, params)
+
+            c = 0
+            for m_row in cur:
+                c += 1
+                self.inspect_match(m_row)
+
+            return c
 
 
-    def __inspect(self, c: Candidate):
+    def inspect_match(self, row):
+
+        c = row_to_candidate(row)
 
         if c.shape.area < settings['MIN_BDG_AREA']:
             self.__refuse(c)
 
-        matches = self.__get_matches(c)
-
-        if len(matches) == 0:
+        if row['match_cnt'] == 0:
             self.__create_new_building(c)
 
-        if len(matches) == 1:
+        if row['match_cnt'] == 1:
             self.__update_building(c)
 
-        if len(matches) > 1:
+        if row['match_cnt'] > 1:
             self.__refuse(c)
+
+    def get_matches_query(self):
+
+        q = "SELECT c.*, count(b.id) as match_cnt " \
+            "from batid_candidate as c " \
+            "left join batid_building as b on ST_Intersects(b.shape, c.shape) " \
+            "and ST_Area(ST_Intersection(b.shape, c.shape)) / ST_Area(c.shape) >= %(min_intersect_ratio)s " \
+            "where c.inspected_at is null  " \
+            "group by c.id " \
+            "limit %(limit)s"
+
+        params = {
+            "min_intersect_ratio": 0.85,
+            "limit": self.BATCH_SIZE
+        }
+
+        return q, params
 
     def __update_building(self, c: Candidate):
 
@@ -63,8 +72,15 @@ class Inspector:
                 "source": c.source
             }
 
+
             with conn.cursor() as cur:
-                cur.execute(q, params)
+                try:
+                    cur.execute(q, params)
+                    conn.commit()
+                except (Exception, psycopg2.DatabaseError) as error:
+                    conn.rollback()
+                    cur.close()
+                    raise error
 
 
             self.__close_inspection(c, 'created_bdg')
@@ -78,7 +94,7 @@ class Inspector:
                 cur.execute(q, {'id': c.id, 'inspect_result': inspect_result})
                 conn.commit()
         # catch db error and rollback
-        except Exception as e:
+        except (Exception, psycopg2.DatabaseError) as error:
             conn.rollback()
             conn.close()
             raise e
@@ -86,26 +102,3 @@ class Inspector:
     def __refuse(self, c):
 
         self.__close_inspection(c, 'refused')
-
-    def __get_matches(self, c: Candidate):
-
-        q = "SELECT b.id as b_id " \
-            "FROM batid_building b " \
-            "WHERE ST_Intersects(b.shape, %(c_shape)s) " \
-            "AND ST_Area(ST_Intersection(b.shape, %(c_shape)s)) / %(c_area)s >= %(min_intersect_ratio)s "
-
-        params = {
-            "c_shape": shapely_to_dbgeom(c.shape),
-            "c_area": c.shape.area,
-            "min_intersect_ratio": 0.85
-        }
-
-        with conn.cursor() as cur:
-            cur.execute(q, params)
-            return cur.fetchall()
-
-
-
-
-
-
