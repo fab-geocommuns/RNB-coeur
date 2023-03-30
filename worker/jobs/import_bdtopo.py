8bc4a3696a2df4f098b3b582815b0dc27345074f
pauletienney <paul.etienney@beta.gouv.fr>
2023-03-30 14:54:41 +0200

    go for full run

--- worker/jobs/import_bdtopo.py
+++ worker/jobs/import_bdtopo.py
@@ -9,7 +9,7 @@ import fiona
 from datetime import datetime, timezone
 import psycopg2
 from db import conn
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor
 
 
 def import_bdtopo(dpt):
@@ -19,8 +19,6 @@ def import_bdtopo(dpt):
     src = Source('bdtopo')
     src.set_param('dpt', dpt)
 
-
-
     with fiona.open(src.find(src.filename)) as f:
         total = len(f)
 
@@ -28,54 +26,36 @@ def import_bdtopo(dpt):
 
         bdgs = []
 
-        sample_size = 50000
-
-        start = time.perf_counter()
-
         executor = ProcessPoolExecutor()
-        for result in executor.map(transform_bdtopo_feature, f[:sample_size], chunksize=10000):
+        for result in executor.map(transform_bdtopo_feature, f, chunksize=10000):
             bdgs.append(result)
 
 
+        buffer_src = Source('buffer', {
+            'folder': 'bdtopo',
+            'filename': 'bdgs-{{dpt}}.csv',
+        })
+        buffer_src.set_param('dpt', dpt)
 
-        # for feature in f:
-        #
-        #     bdg = transform_bdtopo_feature(feature)
-        #     bdgs.append(bdg)
-        #
-        #     if len(bdgs) >= sample_size:
-        #         break
+        cols = bdgs[0].keys()
 
-        end = time.perf_counter()
-        print(f"Elapsed time: {end - start:0.4f} seconds")
+        with open(buffer_src.path, 'w') as f:
+            print("-- writing buffer file --")
+            writer = csv.DictWriter(f, delimiter=';', fieldnames=cols)
+            writer.writerows(bdgs)
 
+        with open(buffer_src.path, 'r') as f, conn.cursor() as cursor:
+            print("-- transfer buffer to db --")
+            try:
+                cursor.copy_from(f, 'batid_candidate', sep=';', columns=cols)
+                conn.commit()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+                conn.rollback()
+                cursor.close()
 
-        # buffer_src = Source('buffer', {
-        #     'folder': 'bdtopo',
-        #     'filename': 'bdgs-{{dpt}}.csv',
-        # })
-        # buffer_src.set_param('dpt', dpt)
-        #
-        # cols = bdgs[0].keys()
-
-        # with open(buffer_src.path, 'w') as f:
-        #     print("-- writing buffer file --")
-        #     writer = csv.DictWriter(f, delimiter=';', fieldnames=cols)
-        #     writer.writerows(bdgs)
-        #
-        # with open(buffer_src.path, 'r') as f, conn.cursor() as cursor:
-        #     print("-- transfer buffer to db --")
-        #     try:
-        #         cursor.copy_from(f, 'batid_candidate', sep=';', columns=cols)
-        #         conn.commit()
-        #     except (Exception, psycopg2.DatabaseError) as error:
-        #         print(error)
-        #         conn.rollback()
-        #         cursor.close()
-        #
-        #
-        # print('- remove buffer')
-        # os.remove(buffer_src.path)
+        print('- remove buffer')
+        os.remove(buffer_src.path)
 
 
 
