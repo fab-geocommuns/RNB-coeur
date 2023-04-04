fad183ab7357229e484b3f3f8eaf25b064fb18c0
fe51 <55736935+fe51@users.noreply.github.com>
2023-04-04 17:00:19 +0200

    style: Applied black

--- app/batid/models.py
+++ app/batid/models.py
@@ -4,19 +4,25 @@ from django.contrib.gis.db import models
 from django.utils.timezone import now
 from django.conf import settings
 
-class Building(models.Model):
 
+class Building(models.Model):
     rnb_id = models.CharField(max_length=17, null=False, unique=True, db_index=True)
     source = models.CharField(max_length=10, null=False)
 
     point = models.PointField(null=True, spatial_index=True, srid=settings.DEFAULT_SRID)
-    shape = models.MultiPolygonField(null=True, spatial_index=True, srid=settings.DEFAULT_SRID)
+    shape = models.MultiPolygonField(
+        null=True, spatial_index=True, srid=settings.DEFAULT_SRID
+    )
 
-    children = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='parents')
-    addresses = models.ManyToManyField('Address', blank=True, related_name='buildings')
+    children = models.ManyToManyField(
+        "self", blank=True, symmetrical=False, related_name="parents"
+    )
+    addresses = models.ManyToManyField("Address", blank=True, related_name="buildings")
 
     ext_bdnb_id = models.CharField(max_length=40, null=True, unique=True, db_index=True)
-    ext_bdtopo_id = models.CharField(max_length=40, null=True, unique=True, db_index=True)
+    ext_bdtopo_id = models.CharField(
+        max_length=40, null=True, unique=True, db_index=True
+    )
 
     def point_geojson(self):
         # todo : is there a better way to go from a PointField to geojson dict ?
@@ -24,12 +30,12 @@ class Building(models.Model):
         return json.loads(self.point.transform(4326, clone=True).geojson)
 
     class Meta:
-        ordering = ['rnb_id']
+        ordering = ["rnb_id"]
+
 
 class Address(models.Model):
-
     id = models.CharField(max_length=40, primary_key=True)
-    source = models.CharField(max_length=10, null=False) # BAN or other origin
+    source = models.CharField(max_length=10, null=False)  # BAN or other origin
 
     street_number = models.CharField(max_length=10, null=True)
     street_rep = models.CharField(max_length=5, null=True)
@@ -37,8 +43,9 @@ class Address(models.Model):
     street_type = models.CharField(max_length=100, null=True)
     city_name = models.CharField(max_length=100, null=True)
     city_zipcode = models.CharField(max_length=5, null=True)
-class Candidate(models.Model):
 
+
+class Candidate(models.Model):
     shape = models.MultiPolygonField(null=True, srid=settings.DEFAULT_SRID)
     source = models.CharField(max_length=20, null=False)
     source_id = models.CharField(max_length=40, null=False)
@@ -47,12 +54,12 @@ class Candidate(models.Model):
     inspected_at = models.DateTimeField(null=True)
     inspect_result = models.CharField(max_length=20, null=True)
 
+
 class City(models.Model):
-    
     id = models.CharField(max_length=40, primary_key=True)
 
     code_insee = models.CharField(max_length=10, null=False)
-    uri_insee = models.CharField(max_length=200,null=True)
+    uri_insee = models.CharField(max_length=200, null=True)
     creation_date = models.DateTimeField(null=False)
-    name = models.CharField(max_length=200,null=False)
-    name_without_article = models.CharField(max_length=200,null=False)
+    name = models.CharField(max_length=200, null=False)
+    name_without_article = models.CharField(max_length=200, null=False)
