abbe3fb9007caf856d7ae50a1f1fac4648d51976
pauletienney <paul.etienney@beta.gouv.fr>
2023-04-04 11:05:39 +0200

    starting enpoints

--- app/api_alpha/serializers.py
+++ app/api_alpha/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import routers, serializers
-from batid.models import Building, Address
+from batid.models import Building, Address, ADS
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ['id', 'source', 'street_number', 'street_rep', 'street_name', 'street_type', 'city_name', 'city_zipcode']
+        fields = [
+            "id",
+            "source",
+            "street_number",
+            "street_rep",
+            "street_name",
+            "street_type",
+            "city_name",
+            "city_zipcode",
+        ]
+
 
 class BuildingSerializer(serializers.ModelSerializer):
-
-    point = serializers.DictField(source='point_geojson')
+    point = serializers.DictField(source="point_geojson")
     addresses = AddressSerializer(many=True, read_only=True)
 
     class Meta:
         model = Building
-        fields = ['rnb_id', 'source', 'point', 'addresses']
+        fields = ["rnb_id", "source", "point", "addresses"]
+
+
+class ADSSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ADS
+        fields = ["issue_number", "issue_date"]
--- app/api_alpha/views.py
+++ app/api_alpha/views.py
@@ -2,24 +2,28 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.exceptions import ParseError
-from batid.models import Building
-from api_alpha.serializers import BuildingSerializer
+from batid.models import Building, ADS
+from api_alpha.serializers import BuildingSerializer, ADSSerializer
 from batid.logic.search import BuildingSearch
 from rest_framework.pagination import PageNumberPagination
 
+
 class BuildingViewSet(ReadOnlyModelViewSet):
-
     def get_queryset(self):
-
         search = BuildingSearch(**self.request.query_params.dict())
 
         if not search.is_valid():
-            raise ParseError({'errors': search.errors})
+            raise ParseError({"errors": search.errors})
             pass
 
         return search.get_queryset()
 
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
-    http_method_names = ['get']
+    http_method_names = ["get"]
     pagination_class = PageNumberPagination
+
+
+class ADSViewSet(ReadOnlyModelViewSet):
+    queryset = ADS.objects.all()
+    serializer_class = ADSSerializer
