367ccd448763fef6a3c82f0f1b23b8550653bbcc
fe51 <55736935+fe51@users.noreply.github.com>
2023-04-13 18:33:44 +0200

    feat: added City serializer & applied black

--- app/api_alpha/serializers.py
+++ app/api_alpha/serializers.py
@@ -1,17 +1,32 @@
+from batid.models import Address, Building, City
 from rest_framework import routers, serializers
-from batid.models import Building, Address
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ['id', 'source', 'street_number', 'street_rep', 'street_name', 'street_type', 'city_name', 'city_zipcode']
+        fields = [
+            "id",
+            "source",
+            "street_number",
+            "street_rep",
+            "street_name",
+            "street_type",
+            "city_name",
+            "city_zipcode",
+        ]
+
 
 class BuildingSerializer(serializers.ModelSerializer):
-
-    point = serializers.DictField(source='point_geojson')
+    point = serializers.DictField(source="point_geojson")
     addresses = AddressSerializer(many=True, read_only=True)
 
     class Meta:
         model = Building
-        fields = ['rnb_id', 'source', 'point', 'addresses']
+        fields = ["rnb_id", "source", "point", "addresses"]
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ["code_insee", "name"]
