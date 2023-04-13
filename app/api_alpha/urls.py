92049b039329c52b413bae127e0de2157e674afb
fe51 <55736935+fe51@users.noreply.github.com>
2023-04-13 18:33:16 +0200

    feat: Added City url endpoint

--- app/api_alpha/urls.py
+++ app/api_alpha/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path, include
-from api_alpha.views import BuildingViewSet
+from api_alpha.views import BuildingViewSet, CityViewSet
+from django.urls import include, path
 from rest_framework import routers
-
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'buildings', BuildingViewSet)
+router.register(r"buildings", BuildingViewSet)
+router.register(r"city", CityViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls))
+    path("", include(router.urls))
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
