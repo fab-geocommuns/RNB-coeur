a07a08b00b817645cc5c62fbb8bd421b2d150217
fe51 <55736935+fe51@users.noreply.github.com>
2023-04-13 18:34:04 +0200

    feat: Added city viewset

--- app/api_alpha/views.py
+++ app/api_alpha/views.py
@@ -1,25 +1,44 @@
+from api_alpha.serializers import BuildingSerializer, CitySerializer
+from batid.logic.search import BuildingSearch
+from batid.models import Building, City
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.exceptions import ParseError
-from batid.models import Building
-from api_alpha.serializers import BuildingSerializer
-from batid.logic.search import BuildingSearch
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
 
 class BuildingViewSet(ReadOnlyModelViewSet):
-
     def get_queryset(self):
-
         search = BuildingSearch(**self.request.query_params.dict())
 
         if not search.is_valid():
-            raise ParseError({'errors': search.errors})
+            raise ParseError({"errors": search.errors})
             pass
 
         return search.get_queryset()
 
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
-    http_method_names = ['get']
+    http_method_names = ["get"]
     pagination_class = PageNumberPagination
+
+
+class CityViewSet(ReadOnlyModelViewSet):
+    """View to look up city matching (name or postal code) with a given input"""
+
+    queryset = City.objects.all()
+
+    def get_queryset(self):
+        query = self.request.query_params.dict()
+        print(query)
+        queryset = City.objects.filter(
+            Q(name__unaccent__icontains=query["txt"])
+            | Q(code_insee__icontains=query["txt"])
+        )
+        return queryset
+
+    serializer_class = CitySerializer
+    http_method_names = ["get"]
+    # pagination_class = PageNumberPagination
