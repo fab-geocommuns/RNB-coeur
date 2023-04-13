2be71087849550656ffd85c8c8a8e4d86561f204
pauletienney <paul.etienney@beta.gouv.fr>
2023-04-13 17:19:22 +0200

    home sweet home

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
+    path("", include("website.urls")),
     path("api/alpha/", include("api_alpha.urls")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
--- app/tests/test_migrations.py
+++
@@ -1,6 +0,0 @@
-
-def test_migration_1_ok():
-    assert True
-
-def test_migration_2_ko():
-    assert True 
