abc43187eb38b4c9b79a3900ba05f93c61ad36d6
pauletienney <paul.etienney@beta.gouv.fr>
2023-04-17 10:13:50 +0200

    skeleton ok

--- app/website/urls.py
+++ app/website/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="web_home"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("ads", TemplateView.as_view(template_name="ads/list.html"), name="ads_list"),
+    path("ads/new", TemplateView.as_view(template_name="ads/new.html"), name="ads_new"),
+    path(
+        "ads/<int:pk>",
+        TemplateView.as_view(template_name="ads/detail.html"),
+        name="ads_detail",
+    ),
 ]
