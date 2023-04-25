07acdc2549e075a89b9da71b4a8dbc95a7892914
pauletienney <paul.etienney@beta.gouv.fr>
2023-04-25 17:15:34 +0200

    change env vars name

--- worker/db.py
+++ worker/db.py
@@ -3,17 +3,22 @@ import os
 from settings import settings
 from shapely import wkb
 
-db_name = os.environ.get("SQL_NAME")
-db_user = os.environ.get("SQL_USER")
-db_password = os.environ.get("SQL_PASSWORD")
-db_host = os.environ.get("SQL_HOST")
-db_port = os.environ.get("SQL_PORT")
+db_name = os.environ.get("POSTGRES_NAME")
+db_user = os.environ.get("POSTGRES_USER")
+db_password = os.environ.get("POSTGRES_PASSWORD")
+db_host = os.environ.get("POSTGRES_HOST")
+db_port = os.environ.get("POSTGRES_PORT")
+
 
 def get_conn():
-    return psycopg2.connect(f"dbname='{db_name}' user='{db_user}' host='{db_host}' password='{db_password}' port='{db_port}'")
+    return psycopg2.connect(
+        f"dbname='{db_name}' user='{db_user}' host='{db_host}' password='{db_password}' port='{db_port}'"
+    )
+
 
 def dbgeom_to_shapely(rowgeom):
     return wkb.loads(rowgeom, hex=True)
+
 
 def shapely_to_dbgeom(shape):
     return wkb.dumps(shape, hex=True, srid=settings["DEFAULT_SRID"])
