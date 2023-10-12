a4141a222e02c6200a97c0371f8aa303f33446cf
pauletienney <paul.etienney@beta.gouv.fr>
2023-10-12 17:01:04 +0200

    added the method to export plots data in village and created a test on "this side of the road" filter

--- app/batid/services/export/village.py
+++ app/batid/services/export/village.py
@@ -8,17 +8,47 @@ from batid.tests.helpers import in_village
 
 @in_village
 def export_village():
+    export_buildings()
+    export_plots()
+
+
+def export_plots():
     cursor = connection.cursor()
     q = """SELECT json_build_object(
-                'type', 'FeatureCollection',
-                'features', json_agg(ST_AsGeoJSON(limited_building.*)::json)
-            ) 
-            FROM (
-                SELECT ST_Transform(b.shape, 4326), b.*, array_agg(json_build_object('address_id', ba.id, 'address', CONCAT(ba.street_number, ' ', ba.street_rep, ' ', ba.street_type, ' ',  ba.street_name, ', ',  ba.city_name))) as add_id FROM batid_building as b
-                join batid_building_addresses bba on bba.building_id = b.id
-                join batid_address ba on bba.address_id = ba.id
-                group by b.id 
-            ) as limited_building;"""
+                        'type', 'FeatureCollection',
+                        'features', json_agg(ST_AsGeoJSON(limited_plots.*)::json)
+                    ) 
+                    FROM (
+                        SELECT ST_Transform(p.shape, 4326), p.* FROM batid_plot as p
+                         
+                    ) as limited_plots;"""
+
+    cursor.execute(q)
+    data = cursor.fetchone()
+
+    source = Source(
+        "village_plots_geojson",
+        custom_ref={"folder": "village", "filename": "village_plots.geojson"},
+    )
+
+    json_obj = json.dumps(data[0], indent=4)
+
+    with open(source.path, "w") as f:
+        f.write(json_obj)
+
+
+def export_buildings():
+    cursor = connection.cursor()
+    q = """SELECT json_build_object(
+                    'type', 'FeatureCollection',
+                    'features', json_agg(ST_AsGeoJSON(limited_building.*)::json)
+                ) 
+                FROM (
+                    SELECT ST_Transform(b.shape, 4326), b.*, array_agg(json_build_object('address_id', ba.id, 'address', CONCAT(ba.street_number, ' ', ba.street_rep, ' ', ba.street_type, ' ',  ba.street_name, ', ',  ba.city_name))) as add_id FROM batid_building as b
+                    join batid_building_addresses bba on bba.building_id = b.id
+                    join batid_address ba on bba.address_id = ba.id
+                    group by b.id 
+                ) as limited_building;"""
 
     cursor.execute(q)
     data = cursor.fetchone()
--- app/batid/tests/test_village.py
+++ app/batid/tests/test_village.py
@@ -12,7 +12,7 @@ from batid.tests.helpers import (
 
 
 class TestSearch(TestCase):
-    fixtures = ["village.json"]
+    fixtures = ["village.json", "village_plots.json"]
 
     def test_search(self):
         assert len(Building.objects.all()) == 137
@@ -69,6 +69,40 @@ class TestSearch(TestCase):
         ban_geocode.assert_called_once()
 
         self.assertEqual(results[0].rnb_id, "78AEVARTSXL6")
+
+    @patch("batid.services.geocoders.BanGeocoder.geocode")
+    @patch("batid.services.geocoders.PhotonGeocoder.geocode")
+    def test_point_and_address(self, photon_geocode, ban_geocode):
+        # We test a point and and address.
+        # The point is close to two buildings, the address should make the difference to find the right building
+
+        ban_geocode.return_value = mock_ban_geocoder_result(
+            id="38185_3240_00002", lng=5.735239, lat=45.17931700000001
+        )
+        photon_geocode.return_value = mock_photon_geocoder_empty_result()
+
+        address = "2  rue germain, Grenoble"
+        point = Point(5.73520463, 45.17937231, srid=4326)
+
+        search = BuildingSearch()
+        search.set_params(address=address, point=point)
+
+        results = search.get_queryset()
+
+        self.assertEqual(results[0].rnb_id, "8JAX6VVU378C")
+
+    def test_on_this_road_side(self):
+        # We test the "on this side of the road" filter on point query
+        # We have a closer building (DK89W1B51UCH) than the expected one but on the wrong side of the road
+
+        point = Point(5.736114236184472, 45.178655320207696, srid=4326)
+
+        search = BuildingSearch()
+        search.set_params(point=point)
+
+        results = search.get_queryset()
+
+        self.assertEqual(results[0].rnb_id, "GTRH2SQNY8G1")
 
     @tag("futuristic_search")
     def test_futuristic_search(self):
