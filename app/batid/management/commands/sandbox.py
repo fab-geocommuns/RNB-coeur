cc87599314e6ed07ac8ed207e893335d9a36f0c5
pauletienney <paul.etienney@beta.gouv.fr>
2023-10-16 14:08:05 +0200

    just add a field to be distinct on 'id' in the public queryset

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,29 +1,9 @@
-from pprint import pprint
-from typing import Optional
 from django.core.management.base import BaseCommand
-import json
-from batid.models import Plot
-from batid.services.geocoders import (
-    GeocodeEarthGeocoder,
-    NominatimGeocoder,
-    PhotonGeocoder,
-)
-from batid.services.imports.import_plots import import_etalab_plots
-from batid.services.search_bdg import BuildingSearch
-
-from django.contrib.gis.geos import GEOSGeometry, Point
-
-from django.db import connection
+from batid.list_bdg import public_bdg_queryset
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        s = BuildingSearch()
-        s.set_params()
+        qs = public_bdg_queryset()
 
-        results = s.get_queryset()
-
-        for b in results:
-            print("---------")
-            print(b.rnb_id)
-            print(b.point_geojson())
+        print(qs[:20].query)
