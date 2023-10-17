a2c4a22608fc6fd0f61ebe20500557fbfc942636
Francis Chabouis <fchabouis@gmail.com>
2023-10-17 15:20:20 +0200

    update tests

--- app/batid/tests/test_import_france_bdgs.py
+++ app/batid/tests/test_import_france_bdgs.py
@@ -6,12 +6,15 @@ from batid.management.commands.import_dpt_bdgs import steps_list
 class ImportFranceTestCase(TestCase):
     def test_create_tasks_from_1(self):
         # create tasks when starting from the first dpt
-        tasks = create_tasks_list_france(1)
-        self.assertEqual(len(tasks), 96 * len(steps_list()))
+        # There are 97 departements in France, excluding overseas territories
+        tasks = create_tasks_list_france("01")
+        self.assertEqual(len(tasks), 97 * len(steps_list()))
+
+        tasks = create_tasks_list_france("")
+        self.assertEqual(len(tasks), 97 * len(steps_list()))
 
     def test_create_tasks_from_custom(self):
-        # create tasks when starting from the first dpt
-        tasks = create_tasks_list_france(90)
+        tasks = create_tasks_list_france("90")
         # expected tasks for 7 departements (90 to 96)
         print(tasks)
         self.assertEqual(len(tasks), 7 * len(steps_list()))
