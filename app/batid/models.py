3d35f9b2be5ae65beca0702ecf494f814a4c3f86
Francis Chabouis <fchabouis@gmail.com>
2023-11-02 14:41:37 +0100

    change shape_wgs84 column : use generic geom
    instead of multipolygon

--- app/batid/models.py
+++ app/batid/models.py
@@ -16,7 +16,7 @@ class Building(models.Model):
     shape = models.MultiPolygonField(
         null=True, spatial_index=True, srid=settings.DEFAULT_SRID
     )
-    shape_wgs84 = models.MultiPolygonField(null=True, srid=4326)
+    shape_wgs84 = models.GeometryField(null=True, spatial_index=True, srid=4326)
 
     addresses = models.ManyToManyField("Address", blank=True, related_name="buildings")
 
