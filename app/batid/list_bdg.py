4bba873374ad68cf03ad7c88803db106caad4804
pauletienney <paul.etienney@beta.gouv.fr>
2023-11-07 17:30:20 +0100

    transformed the query to list buildings for performance issues.

--- app/batid/list_bdg.py
+++ app/batid/list_bdg.py
@@ -4,21 +4,32 @@ from batid.services.bdg_status import BuildingStatus
 from batid.models import Building, City
 
 
-def public_bdg_queryset(user=None) -> QuerySet:
-    allowed_status = BuildingStatus.PUBLIC_TYPES_KEYS
+def list_bdgs(params):
+    qs = Building.objects.all()
 
-    if user and user.is_authenticated:
+    # #######################
+    # Status filter
+
+    # User "permissions" for building status
+    allowed_status = BuildingStatus.PUBLIC_TYPES_KEYS
+    if "user" in params and params["user"].is_authenticated:
         allowed_status = BuildingStatus.ALL_TYPES_KEYS
 
-    return (
-        Building.objects.filter(status__type__in=allowed_status)
-        .order_by("id")
-        .distinct("id")
-    )
+    # Queries status and filter on allowed status
+    status_list = BuildingStatus.DEFAULT_DISPLAY_STATUS
+    if "status" in params:
+        if params["status"] == "all":
+            status_list = BuildingStatus.ALL_TYPES_KEYS
+        else:
+            status_list = params["status"].split(",")
 
+    status = [s for s in status_list if s in allowed_status]
 
-def filter_bdg_queryset(qs: QuerySet, params) -> QuerySet:
-    # Bounding box
+    qs = qs.filter(status__type__in=status, status__is_current=True)
+
+    # #######################
+    # Bounding box filter
+
     bbox_str = params.get("bb", None)
     if bbox_str:
         nw_lat, nw_lng, se_lat, se_lng = [float(coord) for coord in bbox_str.split(",")]
@@ -32,23 +43,14 @@ def filter_bdg_queryset(qs: QuerySet, params) -> QuerySet:
         poly = Polygon(poly_coords, srid=4326)
         qs = qs.filter(shape__intersects=poly)
 
+    # #######################
     # Insee Code filter
+
     insee_code = params.get("insee_code", None)
     if insee_code:
         city = City.objects.get(code_insee=insee_code)
         qs = qs.filter(shape__intersects=city.shape)
 
-    # Status filter
-    status = BuildingStatus.DEFAULT_DISPLAY_STATUS
-
-    query_status_str = params.get("status", None)
-
-    if query_status_str:
-        if query_status_str == "all":
-            status = BuildingStatus.ALL_TYPES_KEYS
-        else:
-            status = query_status_str.split(",")
-
-    qs = qs.filter(status__type__in=status, status__is_current=True)
+    qs = qs.order_by("id")
 
     return qs
