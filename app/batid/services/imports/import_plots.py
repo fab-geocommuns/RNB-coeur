f3b11bc366d31bdebd7742e238ca37d6db5d3ca0
Francis Chabouis <fchabouis@gmail.com>
2023-11-09 11:55:41 +0100

    faster way to do things

--- app/batid/services/imports/import_plots.py
+++ app/batid/services/imports/import_plots.py
@@ -12,6 +12,10 @@ from psycopg2.extras import execute_values
 
 from batid.models import Plot
 from batid.services.source import Source
+from io import StringIO
+
+# import polars as pl
+import csv
 
 
 def import_etalab_plots(dpt: str):
@@ -20,7 +24,7 @@ def import_etalab_plots(dpt: str):
 
     src = Source("plot")
     src.set_param("dpt", dpt)
-    batch_size = 1000000
+    batch_size = 10000
 
     with open(src.path) as f:
         features = ijson.items(f, "features.item", use_float=True)
@@ -29,7 +33,6 @@ def import_etalab_plots(dpt: str):
         c = 0
         for plot in features:
             c += 1
-            print("plot", c)
 
             batch.append(plot)
 
@@ -45,14 +48,51 @@ def __handle_batch(batch):
     print("-- converting batch")
     rows = map(_feature_to_row, batch)
     print("-- saving batch")
-    __save_batch(rows)
+    # print(len(batch))
+
+    f = StringIO()
+    print("-- create DF")
+
+    writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+    writer.writerows(rows)
+
+    # df = pl.DataFrame(rows)
+    print("-- write csv")
+
+    # df.write_csv(f, has_header=False)
+
+    print("-- seaking......")
+
+    f.seek(0)
+
+    print("-- inserting")
+
+    with connection.cursor() as cursor:
+        cursor.copy_from(
+            f,
+            Plot._meta.db_table,
+            columns=("id", "shape", "created_at", "updated_at"),
+            sep=",",
+        )
+
+    print("-- inserted")
+
+    # __save_batch(rows)
+
+
+def csv2string(data):
+    si = StringIO()
+    cw = csv.writer(si)
+    cw.writerow(data)
+    return si.getvalue().strip("\r\n")
 
 
 def __save_batch(batch):
+    print("page size = 10000")
     q = f"INSERT INTO {Plot._meta.db_table} (id, shape, created_at, updated_at) VALUES %s ON CONFLICT DO NOTHING"
 
     with connection.cursor() as cursor:
-        execute_values(cursor, q, batch, page_size=1000000)
+        execute_values(cursor, q, batch, page_size=10000)
 
 
 def polygon_to_multipolygon(polygon):
@@ -65,9 +105,6 @@ def _feature_to_row(feature):
 
     multi_poly = GEOSGeometry(json.dumps(feature["geometry"]))
 
-    if multi_poly.srid != settings.DEFAULT_SRID:
-        multi_poly.transform(settings.DEFAULT_SRID)
-
     if not multi_poly.valid:
         multi_poly = multi_poly.buffer(0)
 
@@ -76,4 +113,4 @@ def _feature_to_row(feature):
 
     now = datetime.now(timezone.utc)
 
-    return feature["id"], f"{multi_poly}", now, now
+    return [feature["id"], multi_poly.hexewkb.decode("ascii"), f"{now}", f"{now}"]
