bd582f94e4b8f918e2111ad13f33506486128b72
pauletienney <paul@donatello.dev>
2023-11-16 10:23:27 +0100

    transform test data from lambert to wgs84

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,10 +1,25 @@
-from django.contrib.gis.geos import Point
+import json
+
+from django.contrib.gis.geos import Point, GEOSGeometry
 from django.core.management.base import BaseCommand
 from batid.list_bdg import list_bdgs
 from batid.services.building import add_default_status
 from batid.services.guess_bdg import BuildingGuess
+from batid.services.vector_tiles import url_params_to_tile, tile_sql
+from batid.tests.test_inspector import get_bdtopo_data
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        add_default_status()
+        rows = get_bdtopo_data()
+
+        for row in rows:
+            print("--")
+            print(row["id"])
+
+            mp = GEOSGeometry(json.dumps(row["geometry"]))
+            mp.srid = 2154
+
+            mp = mp.transform(4326, clone=True)
+
+            print(mp.json)
