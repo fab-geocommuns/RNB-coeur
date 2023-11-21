5a9a6609a1c9301c487d8a5292d9217cf596264e
Francis Chabouis <fchabouis@gmail.com>
2023-11-21 10:35:36 +0100

    create helpers functions

---
+++ app/batid/services/imports/building_import_history.py
@@ -0,0 +1,22 @@
+import uuid
+from batid.models import BuildingImport
+
+
+def insert_building_import(source, bulk_launch_uuid, dpt):
+    building_import = BuildingImport.objects.create(
+        import_source=source,
+        bulk_launch_uuid=bulk_launch_uuid or uuid.uuid4(),
+        departement=dpt,
+        candidate_created_count=0,
+        building_created_count=0,
+        building_updated_count=0,
+        building_refused_count=0,
+    )
+    building_import.save()
+    return
+
+
+def increment_created_candidates(building_import, candidates_count):
+    building_import.candidate_created_count += candidates_count
+    building_import.save()
+    return building_import
