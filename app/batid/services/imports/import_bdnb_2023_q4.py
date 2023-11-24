af9be754d1fa5a29f4fbfbf6aa1d3d8053b10103
pauletienney <paul@donatello.dev>
2023-11-24 10:17:01 +0100

    start to write the import

--- app/batid/services/imports/import_bdnb_2023_q4.py
+++ app/batid/services/imports/import_bdnb_2023_q4.py
@@ -1,4 +1,20 @@
-def import_bdnd_2023_q4_bdgs():
+import csv
+
+from batid.services.source import Source
+
+
+def import_bdnd_2023_q4_bdgs(dpt):
+    print("## Import BDNB 2023 Q4 buildings")
+
+    src = Source("bdnb_2023_q4")
+    src.set_param("dpt", dpt)
+
+    candidates = []
+
+    with open(src.find(f"{dpt}_bdgs.csv"), "r") as f:
+        print("- list buildings")
+        reader = csv.DictReader(f, delimiter=";")
+
     pass
 
 
