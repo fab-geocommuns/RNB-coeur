8250b369f059301017535ee977e004ac88dcec28
Francis Chabouis <fchabouis@gmail.com>
2023-12-05 15:23:05 +0100

    delete field and update model

---
+++ app/batid/migrations/utils/create_view.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+def sql_migration_building_with_history():
+    return migrations.RunSQL(
+        """
+            -- create view
+            create view batid_building_with_history as
+            select null as bh_id, * from batid_building
+            union all
+            select * from batid_building_history;
+            """,
+        reverse_sql="""
+            DROP VIEW IF EXISTS batid_building_with_history;
+            """,
+    )
