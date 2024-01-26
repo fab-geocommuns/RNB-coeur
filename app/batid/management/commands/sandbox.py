b42e593218b2d5aa2b5f37f29bff85bc3ed64c2b
pauletienney <paul@donatello.dev>
2024-01-26 10:06:46 +0100

    accélération multithread + nettoyage de fichiers

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -10,6 +10,7 @@ from shapely.geometry import shape
 
 from batid.models import Building, Candidate
 from batid.services.candidate import Inspector
+from batid.services.closest_bdg import get_closest
 from batid.services.source import Source, bdtopo_source_switcher
 from batid.services.imports.import_bdtopo import import_bdtopo
 from batid.tasks import dl_source
@@ -18,14 +19,8 @@ from batid.utils.geo import fix_nested_shells
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        dpt = "976"
-        bdtopo_edition = "bdtopo_2023_09"
+        lat, lng = 44.777823375105186, -0.4532151396778606
+        radius = 20
+        qs = get_closest(lat, lng, radius)
 
-        source_name = bdtopo_source_switcher(bdtopo_edition, dpt)
-
-        print(source_name)
-
-        src = Source(source_name)
-        src.set_param("dpt", dpt)
-
-        print(f"-- downloading {src.url}")
+        pprint(qs.first().rnb_id)
