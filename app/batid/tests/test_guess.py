f48cc49d3ce53a2fd0fbd0e1ac9125b86ce458c4
pauletienney <paul@donatello.dev>
2024-03-04 12:32:10 +0100

    début écriture des tests

---
+++ app/batid/tests/test_guess.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+
+class TestGuesser(TestCase):
+    def test_work_file_creation(self):
+        pass
+
+    def test_work_file_loading(self):
+        pass
+
+    def test_guess_from_closest(self):
+        pass
+
+    def test_row_validation(self):
+        # todo : tester les cas d'erreurs (surement besoin de plusieurs fonctions)
+        pass
+
+    def test_guess_from_address(self):
+        pass
+
+    def test_guest_from_name(self):
+        pass
