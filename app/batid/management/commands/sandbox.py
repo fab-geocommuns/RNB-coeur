e6540392641a08edcb297696f8c8bb3d189044dd
Francis Chabouis <fchabouis@gmail.com>
2024-03-13 14:12:41 +0100

    add useless import

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 
 from batid.services.guess_bdg_new import Guesser
 from batid.utils.misc import is_float
+import json
 
 
 class Command(BaseCommand):
