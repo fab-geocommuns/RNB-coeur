7d2caab8c579506e4273f7c723450d4b89dd9deb
Francis Chabouis <fchabouis@gmail.com>
2024-03-13 14:26:40 +0100

    another commit

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 from batid.services.guess_bdg_new import Guesser
 from batid.utils.misc import is_float
 import json
+import requests
 
 
 class Command(BaseCommand):
