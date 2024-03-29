1e08648ee70f77f70ae8a72344dcf6c1570d336e
pauletienney <paul@donatello.dev>
2024-03-29 10:20:56 +0100

    use already existing db router instead of new one

--- app/app/dbrouters.py
+++ app/app/dbrouters.py
@@ -1,10 +1,18 @@
 # app/dbrouters.py
 from batid.models import BuildingHistoryOnly
 from batid.models import BuildingWithHistory
+from batid.models import Guess
 
 
 class DBRouter(object):
+    def db_for_read(self, model, **hints):
+        if model == Guess:
+            return "guess"
+        return None
+
     def db_for_write(self, model, **hints):
         if model == BuildingWithHistory or model == BuildingHistoryOnly:
             raise Exception("BuildingWithHistory model is read only!")
+        if model == Guess:
+            return "guess"
         return None
