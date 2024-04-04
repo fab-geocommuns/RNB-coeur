8aa7fbf788369196bd4d8b1634bb8cfe14223516
Francis Chabouis <fchabouis@gmail.com>
2024-04-04 12:20:18 +0200

    keep redoc only, as it looks better

--- app/api_alpha/urls.py
+++ app/api_alpha/urls.py
@@ -2,7 +2,6 @@ from django.urls import include
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularRedocView
-from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework import routers
 from rest_framework.authtoken import views as auth_views
 
@@ -27,12 +26,6 @@ router.register(r"ads", ADSViewSet)
 urlpatterns = [
     # YOUR PATTERNS
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Optional UI:
-    path(
-        "schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
     path(
         "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
