519b164f788770ef050f462dff5c71312fdae256
Francis Chabouis <fchabouis@gmail.com>
2024-05-06 15:40:54 +0200

    test delete one address linked to many buildings

--- app/batid/tests/test_building_addresse_link_trigger.py
+++ app/batid/tests/test_building_addresse_link_trigger.py
@@ -74,19 +74,29 @@ class AddressDeletionTrigger(TestCase):
         a1 = Address.objects.create(id="address_1")
         a2 = Address.objects.create(id="address_2")
 
-        b = Building.objects.create(rnb_id="1", addresses_id=[a1.id, a2.id])
+        b1 = Building.objects.create(rnb_id="1", addresses_id=[a1.id, a2.id])
+        b2 = Building.objects.create(rnb_id="2", addresses_id=[a1.id])
+        b3 = Building.objects.create(rnb_id="3", addresses_id=[a2.id])
 
         links_n = BuildingAddressesReadOnly.objects.count()
-        self.assertEqual(links_n, 2)
+        self.assertEqual(links_n, 4)
 
         # the address is deleted
         a1.delete()
 
         # the building should have been updated by the trigger, only a2 should be left
-        b = Building.objects.get(rnb_id="1")
-        self.assertEqual(b.addresses_id, [a2.id])
+        b1 = Building.objects.get(rnb_id="1")
+        self.assertEqual(b1.addresses_id, [a2.id])
 
-        links = BuildingAddressesReadOnly.objects.all()
-        self.assertEqual(links.count(), 1)
-        self.assertEqual(links[0].building_id, b.id)
+        b2 = Building.objects.get(rnb_id="2")
+        self.assertEqual(b2.addresses_id, [])
+
+        b3 = Building.objects.get(rnb_id="3")
+        self.assertEqual(b3.addresses_id, [a2.id])
+
+        links = BuildingAddressesReadOnly.objects.all().order_by("building_id")
+        self.assertEqual(links.count(), 2)
+        self.assertEqual(links[0].building_id, b1.id)
         self.assertEqual(links[0].address_id, a2.id)
+        self.assertEqual(links[1].building_id, b3.id)
+        self.assertEqual(links[1].address_id, a2.id)
