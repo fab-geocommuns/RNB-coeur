e65b24306449554d439198a6280c9f578a11a2ae
Francis Chabouis <fchabouis@gmail.com>
2024-05-27 16:07:09 +0200

    fix test_upload_to_s3

--- app/batid/tests/test_data_gouv_publication.py
+++ app/batid/tests/test_data_gouv_publication.py
@@ -128,6 +128,7 @@ class TestDataGouvPublication(TestCase):
             "S3_SCALEWAY_REGION_NAME": "us-east-1",
             "S3_SCALEWAY_ENDPOINT_URL": "https://s3.us-east-1.amazonaws.com",
             "S3_SCALEWAY_BUCKET_NAME": "rnb-opendata",
+            "S3_SCALEWAY_OPENDATA_DIRECTORY": "files",
         },
     )
     def test_upload_to_s3(self):
