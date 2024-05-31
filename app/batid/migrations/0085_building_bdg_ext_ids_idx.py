e3a28b4311f176d72c92a6999d7da22d018d8ac0
pauletienney <paul@donatello.dev>
2024-05-31 17:33:58 +0200

    fix multiple leaves error in migrations files order

--- app/batid/migrations/0085_building_bdg_ext_ids_idx.py
+++ app/batid/migrations/0085_building_bdg_ext_ids_idx.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("batid", "0083_building_bdg_sys_period_idx_and_more"),
+        ("batid", "0084_alter_building_event_type_and_more"),
     ]
 
     operations = [
