d2fb91b781c91692bf97cf3f1e7bf3cf83247956
Romain MAZIÈRE (SIGMAZ Consilium) <romain.maziere@sigmaz-consilium.fr>
2024-06-06 14:57:26 +0200

    Add start-at option to publish_opendata

--- app/batid/management/commands/publish_opendata.py
+++ app/batid/management/commands/publish_opendata.py
@@ -14,16 +14,29 @@ class Command(BaseCommand):
             type=str,
             default="department",
             choices=["country", "department"],
+        ), 
+        parser.add_argument(
+            "--start-at",
+            type=str,
+            help="Only for strate=department, allow to start at a specific department code.",
         )
 
     def handle(self, *args, **options):
         strate = options["strate"]
-        enqueue_tasks(strate)
+        if options["start-at"]:
+            starting_code = options["start-at"]
+        else:
+            starting_code = None
+        enqueue_tasks(strate, starting_code)
 
 
-def enqueue_tasks(strate):
+def enqueue_tasks(strate, starting_code=None):
     if strate == "country":
         app.send_task("batid.tasks.opendata_publish_national")
     elif strate == "department":
+        process = False
         for dept in dpts_list():
-            app.send_task("batid.tasks.opendata_publish_department", args=[dept])
+            if dept == starting_code:
+                process = True
+            if starting_code is None or process is True:
+                app.send_task("batid.tasks.opendata_publish_department", args=[dept])
