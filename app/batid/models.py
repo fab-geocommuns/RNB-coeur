e66599c34db3350d5155fd66c533b7acaa77f48f
pauletienney <paul@donatello.dev>
2024-06-06 10:40:01 +0200

    index concurrently + remove default order

--- app/batid/models.py
+++ app/batid/models.py
@@ -143,7 +143,7 @@ class Building(BuildingAbstract):
         return self.point_geojson()["coordinates"][0]
 
     class Meta:
-        ordering = ["rnb_id"]
+        # ordering = ["rnb_id"]
         indexes = [
             GinIndex(fields=["event_origin"], name="bdg_event_origin_idx"),
             GinIndex(fields=["addresses_id"], name="bdg_addresses_id_idx"),
