4988c10ee97487b1c4929420ed89e56fabe67e6e
pauletienney <paul@donatello.dev>
2024-06-27 08:27:27 +0200

    merging

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,11 +1,19 @@
 from celery.canvas import chain
 from django.core.management.base import BaseCommand
 
-from batid.services.imports.import_bdtopo import create_bdtopo_dpt_import_tasks
+from batid.services.imports.import_bdtopo import (
+    create_bdtopo_dpt_import_tasks,
+    bdtopo_src_params,
+)
+from batid.services.source import Source
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        tasks = create_bdtopo_dpt_import_tasks("48")
-        chain(*tasks)()
+        params = bdtopo_src_params("75", "2023-06-15")
+
+        src = Source("bdtopo")
+        src.set_params(params)
+
+        print(src.uncompress_abs_dir)
