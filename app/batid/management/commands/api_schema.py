8896b6ecfb9ebba790c2704fab5122169a3a8b41
pauletienney <paul@donatello.dev>
2024-09-10 11:21:15 +0200

    add a command to display the schema. It can also validate it if wanted.

---
+++ app/batid/management/commands/api_schema.py
@@ -0,0 +1,41 @@
+import json
+
+
+from django.core.management.base import BaseCommand
+from openapi_spec_validator import validate
+from openapi_spec_validator.validation.exceptions import OpenAPIValidationError
+
+from api_alpha.utils.rnb_doc import build_schema_dict
+
+
+class Command(BaseCommand):
+
+    # add a boolean option to validate or note the schema
+    def add_arguments(self, parser):
+        parser.add_argument("--validate", action="store_true", default=False)
+
+
+    def handle(self, *args, **options):
+
+        schema_dict = build_schema_dict()
+        print(json.dumps(schema_dict, indent=4, ensure_ascii=False))
+
+
+
+        if options["validate"]:
+
+            print('-------- Validate schema --------')
+            try:
+                validate(schema_dict)
+                print("> Schema is valid 🎉")
+            except OpenAPIValidationError as e:
+                print(f"> Schema is NOT valid: {e}")
+                raise e
+
+
+
+
+
+
+
+
