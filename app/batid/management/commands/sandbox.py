e3a02540a40457fe6b4c82f70432779ba098a7db
pauletienney <paul@donatello.dev>
2024-09-23 12:00:09 +0200

    add sandbox to test in prod

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,14 +1,15 @@
+import os
+
 from django.core.management.base import BaseCommand
 
 from api_alpha.utils.rnb_doc import build_schema_dict
+from batid.services.mattermost import notify_tech, _notifications_are_active
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
-        schema = build_schema_dict()
+        print(os.environ.get("MATTERMOST_RNB_TECH_WEBHOOK_URL"))
+        print(_notifications_are_active())
 
-        # then, we should convert to YML
-        # it might be useful to check it against an OpenAPI schema validator (or it can be done in test)
-
-        print(schema)
+        notify_tech("Test notification")
