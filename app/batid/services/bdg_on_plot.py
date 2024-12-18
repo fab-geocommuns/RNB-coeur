a4885871b4e02de4702d75eb4b2a7df900fe93ff
Francis Chabouis <fchabouis@gmail.com>
2024-12-18 12:14:07 +0100

    add API endpoint for buildings on plot

---
+++ app/batid/services/bdg_on_plot.py
@@ -0,0 +1,36 @@
+from django.contrib.gis.db.models.functions import Area
+from django.contrib.gis.db.models.functions import Intersection
+from django.db.models import Case
+from django.db.models import F
+from django.db.models import Func
+from django.db.models import When
+from django.db.models.lookups import Exact
+
+from batid.exceptions import PlotUnknown
+from batid.models import Building
+from batid.models import Plot
+
+
+def get_buildings_on_plot(plot_id: str):
+    try:
+        plot = Plot.objects.get(id=plot_id)
+    except Plot.DoesNotExist:
+        raise PlotUnknown(f"plot id {plot_id} is unknown by the RNB")
+
+    bdg_qs = (
+        Building.objects.filter(shape__intersects=plot.shape)
+        .annotate(
+            bdg_cover_ratio=Case(
+                When(
+                    Exact(
+                        Func(F("shape"), function="ST_AREA"),
+                        0,
+                    ),
+                    then=1.0,
+                ),
+                default=Area(Intersection("shape", plot.shape)) / Area("shape"),
+            )
+        )
+        .order_by("-bdg_cover_ratio")
+    )
+    return bdg_qs
