4a39241713eb869a6d1ee74a51269560d1689ed6
pauletienney <paul@donatello.dev>
2025-02-24 15:41:04 +0100

    clean sandbox

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -4,6 +4,4 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
-        "https://data.geopf.fr/telechargement/download/BDTOPO/BDTOPO_3-4_TOUSTHEMES_SHP_LAMB93_D986_2024-12-15/BDTOPO_3-4_TOUSTHEMES_SHP_LAMB93_D986_2024-12-15.7z"
-
         pass
