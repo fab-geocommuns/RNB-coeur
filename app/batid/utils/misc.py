27791bca771bd6936f3f4e1a6eb8c2dd13fc982a
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-03-20 21:38:15 +0100

    Take highest scoring BAN address

--- app/batid/utils/misc.py
+++ app/batid/utils/misc.py
@@ -1,6 +1,22 @@
+from collections.abc import Iterable
+from itertools import groupby
+from typing import Any, Callable
+
+
 def is_float(string: str) -> bool:
     try:
         float(string)
         return True
     except ValueError:
         return False
+
+
+def max_by_group(
+    iterable: Iterable, max_key: Callable, group_key: Callable
+) -> dict[str, Any]:
+    result: dict[str, Any] = {}
+    for item in iterable:
+        group = group_key(item)
+        if group not in result or max_key(result[group]) < max_key(item):
+            result[group] = item
+    return result
