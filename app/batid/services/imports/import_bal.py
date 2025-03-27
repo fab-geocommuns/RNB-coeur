339b4cbc5c62e501059c8495856a41098d7568b1
pauletienney <paul@donatello.dev>
2025-03-27 15:29:06 +0100

    all tasks

--- app/batid/services/imports/import_bal.py
+++ app/batid/services/imports/import_bal.py
@@ -1,3 +1,9 @@
 def create_all_bal_links_tasks(dpts: list):
 
-    return []
+    tasks = []
+
+    for dpt in dpts:
+        dpt_tasks = _create_bal_links_dpt_tasks(dpt)
+        tasks.extend(dpt_tasks)
+
+    return tasks
