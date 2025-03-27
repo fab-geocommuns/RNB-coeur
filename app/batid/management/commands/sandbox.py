c4f724689dedd267b962431328a8c287e996324c
pauletienney <paul@donatello.dev>
2025-03-27 12:21:42 +0100

    clean sandbox

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,18 +1,7 @@
-from django.conf import settings
 from django.core.management.base import BaseCommand
-
-from batid.services.imports.import_ban import import_ban_addresses
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
-
-
-        # src = Source("ban")
-        # src.set_params({"dpt": "75"})
-
-        # src.download()
-        # src.uncompress()
-
-        # import_ban_addresses({"dpt": "75"})
+        pass
