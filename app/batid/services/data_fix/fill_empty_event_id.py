3849b7b0dc6c2bce89882dc8fca2a25d98412dd5
pauletienney <paul@donatello.dev>
2025-06-04 16:55:03 +0200

    history seems done

--- app/batid/services/data_fix/fill_empty_event_id.py
+++ app/batid/services/data_fix/fill_empty_event_id.py
@@ -4,20 +4,32 @@ from django.db import transaction
 from batid.models import BuildingHistoryOnly
 
 
-def _fetch_history_rows(batch_size=10_000):
+def _fetch_old_rows(batch_size=10_000):
 
     # Django select for update
     return BuildingHistoryOnly.objects.select_for_update(skip_locked=True).filter(
-        event_id=None, event_type__in=[None, "creation", "update"]
+        event_id=None, event_type__in=["creation", "update"]
     )[:batch_size]
 
 
-def fill_empty_event_id():
+def fill_empty_event_id() -> bool:
+
+    updated_some_rows = False
 
     # Do past versions first
     with transaction.atomic():
-        while True:
-            rows = _fetch_history_rows()
-            for row in rows:
+
+        old_rows = _fetch_old_rows()
+
+        if old_rows:
+
+            # Assign a new UUID to each ro
+            for row in old_rows:
                 row.event_id = uuid.uuid4()
-                row.save()
+
+            # Save in bulk to avoid multiple queries
+            BuildingHistoryOnly.objects.bulk_update(old_rows, ["event_id"])
+
+            updated_some_rows = True
+
+    return updated_some_rows
--- app/batid/tests/data_fix/test_fix_missing_event_id.py
+++ app/batid/tests/data_fix/test_fix_missing_event_id.py
@@ -8,30 +8,73 @@ class TestMissingEventId(TestCase):
 
     def setUp(self):
 
+        # The building to fill
+
         b = Building.objects.create(
             rnb_id="one",
             status="constructed",
             shape="POINT(1 1)",
             is_active=True,
+            event_type="creation",
             event_id=None,  # Missing event_id
         )
 
+        b.event_type = "update"
         b.status = "demolished"
         b.save()
 
+        # Two buildings with merge and split event_type
+        # They must not be fixed by the fill_empty_event_id function
+        # Merge and split are more complex operations concerning multiple buildings
+        # All those buildings should have the same event_id
+        # The fill_empty_event_id function handles buildings one by one
+
+        Building.objects.create(
+            rnb_id="merge",
+            status="constructed",
+            shape="POINT(1 1)",
+            is_active=True,
+            event_type="merge",
+            event_id=None,  # Missing event_id
+        )
+
+        Building.objects.create(
+            rnb_id="split",
+            status="constructed",
+            shape="POINT(1 1)",
+            is_active=True,
+            event_type="merge",
+            event_id=None,  # Missing event_id
+        )
+
     def test_missing_event_id(self):
 
+        # Verify some values before the fix
         versions_count = BuildingWithHistory.objects.all().count()
-        self.assertEqual(versions_count, 2)
+        self.assertEqual(versions_count, 4)
 
         past_version = BuildingHistoryOnly.objects.get(rnb_id="one")
         self.assertIsNone(past_version.event_id)
+        old_updated_at = past_version.updated_at
 
+        # ###### Trigger the fix ######
         # Correct the missing event_ids
         fill_empty_event_id()
 
+        # The history trigger should have been disabled. The total number of versions should remain the same
         versions_count = BuildingWithHistory.objects.all().count()
-        self.assertEqual(versions_count, 2)
+        self.assertEqual(versions_count, 4)
 
+        # The past version should now have an event_id
         past_version = BuildingHistoryOnly.objects.get(rnb_id="one")
         self.assertIsNotNone(past_version.event_id)
+
+        # We want the updated_at value to remain unchanged despite the auto_now=True field setting
+        new_updated_at = past_version.updated_at
+        self.assertEqual(old_updated_at, new_updated_at)
+
+        # The merge and split buildings should not have been modified
+        merge_building = Building.objects.get(rnb_id="merge")
+        self.assertIsNone(merge_building.event_id)
+        split_building = Building.objects.get(rnb_id="split")
+        self.assertIsNone(split_building.event_id)
