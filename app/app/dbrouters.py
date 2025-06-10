11b22535f04532d9859d7b99ff66a43df8864f4f
pauletienney <paul@donatello.dev>
2025-06-10 11:00:16 +0200

    reinstall dbrouter protection

--- app/app/dbrouters.py
+++ app/app/dbrouters.py
@@ -4,9 +4,9 @@ from batid.models import BuildingAddressesReadOnly
 
 class DBRouter(object):
     def db_for_write(self, model, **hints):
-        # TEMPORARY DISABLED PROTECTION: fill_empty_event_id
-        # if model == BuildingWithHistory or model == BuildingHistoryOnly:
-        #     raise Exception("BuildingWithHistory model is read only!")
+
+        if model == BuildingWithHistory or model == BuildingHistoryOnly:
+            raise Exception("BuildingWithHistory model is read only!")
         if model == BuildingAddressesReadOnly:
             raise Exception(
                 "BuildingAddressesReadOnly model is read only, as the name suggests!"
