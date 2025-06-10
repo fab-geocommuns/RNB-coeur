50fc8148a6cb5d9a276b06fde7789dadd69d7e02
pauletienney <paul@donatello.dev>
2025-06-10 11:03:07 +0200

    unplung test for event type

--- app/batid/tests/data_fix/test_fix_missing_event_type.py
+++ app/batid/tests/data_fix/test_fix_missing_event_type.py
@@ -1,142 +1,145 @@
-from datetime import timedelta
-from django.utils import timezone
+# Thoses were used only during the development and execution of the fill_event_type fix
+# They cannot be used in normal situation since they require to disable the write protection set in dbrouters.py
 
-from django.test import TransactionTestCase, TestCase
+# from datetime import timedelta
+# from django.utils import timezone
 
-from batid.services.data_fix.fill_empty_event_type import _fetch, fill_empty_event_type
-from batid.models import Building
-from batid.models import BuildingHistoryOnly
+# from django.test import TransactionTestCase, TestCase
 
-
-class TestFix(TestCase):
-
-    def setUp(self):
-
-        # One with empty Building and BuildingHistoryOnly
-        b = Building.objects.create(
-            rnb_id="one",
-            status="constructed",
-            shape="POINT(1 1)",
-            is_active=True,
-            event_type=None,  # Missing event_type
-        )
-
-        b.status = "demolished"
-        b.save()
-
-        # Another one with event_type filled
-        b = Building.objects.create(
-            rnb_id="two",
-            status="constructed",
-            shape="POINT(1 1)",
-            is_active=True,
-            event_type="creation",  # Filled event_type
-        )
-
-        b.shape = "POINT(2 2)"
-        b.event_type = "update"
-        b.save()
-
-    def test_fetch(self):
-
-        rows = _fetch(10)
-
-        self.assertEqual(len(rows), 1)
-        self.assertIsNone(rows[0].event_type)
-        self.assertIsInstance(rows[0], BuildingHistoryOnly)
-
-    def fix(self):
-
-        updated_rows = fill_empty_event_type(10)
-
-        self.assertEqual(updated_rows, 1)
-
-        row = BuildingHistoryOnly.objects.get(rnb_id="one")
-        self.assertEqual(row.event_type, "creation")
+# from batid.services.data_fix.fill_empty_event_type import _fetch, fill_empty_event_type
+# from batid.models import Building
+# from batid.models import BuildingHistoryOnly
 
 
-class TestFixCreationOnly(TestCase):
+# class TestFix(TestCase):
 
-    def setUp(self):
+#     def setUp(self):
 
-        # We will create two history rows with event_type=None
-        # One will be creation (created_at - lower(sys_period) < 2 seconds )
-        # The other will be update (created_at - lower(sys_period) > 2 seconds)
-        # The first one should be fixed, the second one should not
+#         # One with empty Building and BuildingHistoryOnly
+#         b = Building.objects.create(
+#             rnb_id="one",
+#             status="constructed",
+#             shape="POINT(1 1)",
+#             is_active=True,
+#             event_type=None,  # Missing event_type
+#         )
 
-        # The building to fill/fix
-        b = Building.objects.create(
-            rnb_id="three",
-            status="constructed",
-            shape="POINT(1 1)",
-            is_active=True,
-            event_type=None,  # Missing event_type
-        )
+#         b.status = "demolished"
+#         b.save()
 
-        b.status = "demolished"
-        b.save()
+#         # Another one with event_type filled
+#         b = Building.objects.create(
+#             rnb_id="two",
+#             status="constructed",
+#             shape="POINT(1 1)",
+#             is_active=True,
+#             event_type="creation",  # Filled event_type
+#         )
 
-        # The building to "old" to be fixed
-        old_b = Building.objects.create(
-            rnb_id="three_old",
-            status="constructed",
-            shape="POINT(1 1)",
-            is_active=True,
-            event_type=None,  # Missing event_type
-        )
+#         b.shape = "POINT(2 2)"
+#         b.event_type = "update"
+#         b.save()
 
-        old_b.status = "demolished"
-        old_b.save()
+#     def test_fetch(self):
 
-        # We now have two versionsof the building.
-        # We artifically set the created_at to be older than 2 seconds
+#         rows = _fetch(10)
 
-        old_creation_row = BuildingHistoryOnly.objects.get(rnb_id="three_old")
+#         self.assertEqual(len(rows), 1)
+#         self.assertIsNone(rows[0].event_type)
+#         self.assertIsInstance(rows[0], BuildingHistoryOnly)
 
-        old_creation_row.created_at = old_creation_row.sys_period.lower + timedelta(
-            seconds=3
-        )
-        old_creation_row.save()
+#     def fix(self):
 
-    def test_fix_creation_only(self):
+#         updated_rows = fill_empty_event_type(10)
 
-        all_history_rows = BuildingHistoryOnly.objects.all()
-        self.assertEqual(len(all_history_rows), 2)
+#         self.assertEqual(updated_rows, 1)
 
-        updated_rows = fill_empty_event_type(10)
-
-        self.assertEqual(updated_rows, 1)
-
-        old_creation_row = BuildingHistoryOnly.objects.get(rnb_id="three_old")
-        self.assertIsNone(old_creation_row.event_type)
+#         row = BuildingHistoryOnly.objects.get(rnb_id="one")
+#         self.assertEqual(row.event_type, "creation")
 
 
-class TestBatchSize(TestCase):
+# class TestFixCreationOnly(TestCase):
 
-    def setUp(self):
+#     def setUp(self):
 
-        b = Building.objects.create(
-            rnb_id="batch_test",
-            status="constructed",
-            shape="POINT(1 1)",
-            is_active=True,
-            event_type=None,  # Missing event_type
-        )
+#         # We will create two history rows with event_type=None
+#         # One will be creation (created_at - lower(sys_period) < 2 seconds )
+#         # The other will be update (created_at - lower(sys_period) > 2 seconds)
+#         # The first one should be fixed, the second one should not
 
-        b.status = "demolished"
-        b.save()
+#         # The building to fill/fix
+#         b = Building.objects.create(
+#             rnb_id="three",
+#             status="constructed",
+#             shape="POINT(1 1)",
+#             is_active=True,
+#             event_type=None,  # Missing event_type
+#         )
 
-        b = Building.objects.create(
-            rnb_id="batch_test_2",
-            status="constructed",
-            shape="POINT(2 2)",
-            is_active=True,
-            event_type=None,  # Missing event_type
-        )
-        b.status = "demolished"
-        b.save()
+#         b.status = "demolished"
+#         b.save()
 
-    def test_batch_size(self):
+#         # The building to "old" to be fixed
+#         old_b = Building.objects.create(
+#             rnb_id="three_old",
+#             status="constructed",
+#             shape="POINT(1 1)",
+#             is_active=True,
+#             event_type=None,  # Missing event_type
+#         )
 
-        updated_rows = fill_empty_event_type(batch_size=1)
-        self.assertEqual(updated_rows, 1)
+#         old_b.status = "demolished"
+#         old_b.save()
+
+#         # We now have two versionsof the building.
+#         # We artifically set the created_at to be older than 2 seconds
+
+#         old_creation_row = BuildingHistoryOnly.objects.get(rnb_id="three_old")
+
+#         old_creation_row.created_at = old_creation_row.sys_period.lower + timedelta(
+#             seconds=3
+#         )
+#         old_creation_row.save()
+
+#     def test_fix_creation_only(self):
+
+#         all_history_rows = BuildingHistoryOnly.objects.all()
+#         self.assertEqual(len(all_history_rows), 2)
+
+#         updated_rows = fill_empty_event_type(10)
+
+#         self.assertEqual(updated_rows, 1)
+
+#         old_creation_row = BuildingHistoryOnly.objects.get(rnb_id="three_old")
+#         self.assertIsNone(old_creation_row.event_type)
+
+
+# class TestBatchSize(TestCase):
+
+#     def setUp(self):
+
+#         b = Building.objects.create(
+#             rnb_id="batch_test",
+#             status="constructed",
+#             shape="POINT(1 1)",
+#             is_active=True,
+#             event_type=None,  # Missing event_type
+#         )
+
+#         b.status = "demolished"
+#         b.save()
+
+#         b = Building.objects.create(
+#             rnb_id="batch_test_2",
+#             status="constructed",
+#             shape="POINT(2 2)",
+#             is_active=True,
+#             event_type=None,  # Missing event_type
+#         )
+#         b.status = "demolished"
+#         b.save()
+
+#     def test_batch_size(self):
+
+#         updated_rows = fill_empty_event_type(batch_size=1)
+#         self.assertEqual(updated_rows, 1)
