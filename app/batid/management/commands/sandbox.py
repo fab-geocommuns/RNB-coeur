482648283e679c42eb2c77e616e3f61e97f93700
pauletienney <paul@donatello.dev>
2025-06-18 17:08:35 +0200

    wip

--- app/batid/management/commands/sandbox.py
+++ app/batid/management/commands/sandbox.py
@@ -1,7 +1,16 @@
+import time
 from django.core.management.base import BaseCommand
+
+from batid.services.data_fix.fill_empty_event_id import fill_empty_event_id
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+
+        start = time.perf_counter()
+        c = fill_empty_event_id(1000)
+        end = time.perf_counter()
+        print(f"Time taken: {end - start:.2f} seconds")
+        print(f"Updated {c} rows with empty event_id")
 
         pass
