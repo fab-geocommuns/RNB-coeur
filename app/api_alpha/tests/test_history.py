ecd1995e4ece5e3bb479b8a44fcf61f3254824bf
pauletienney <paul@donatello.dev>
2025-07-22 15:36:55 +0200

    test deactivation

--- app/api_alpha/tests/test_history.py
+++ app/api_alpha/tests/test_history.py
@@ -519,6 +519,34 @@ class SingleBuildingHistoryTest(APITestCase):
             data[0]["event"]["origin"]["details"]["description"], "Test data fix"
         )
 
+    def test_deactivate_reactivate(self):
+        """
+        Deactivate specfics to test:
+        - event type is 'deactivation'
+        - event.details.updated_fields contains 'is_active'
+
+        Reactivate specifics to test:
+        - event type is 'reactivation'
+        - event.details.updated_fields contains 'is_active'
+        """
+
+        # We deactivate the building
+        self.client.patch(
+            f"/api/alpha/buildings/{self.rnb_id}/",
+            data=json.dumps({"is_active": False}),
+            content_type="application/json",
+        )
+
+        # Then we check for the history
+        r = self.client.get(f"/api/alpha/buildings/{self.rnb_id}/history/")
+        data = r.json()
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]["event"]["type"], "deactivation")
+        self.assertListEqual(
+            data[0]["event"]["details"]["updated_fields"], ["is_active"]
+        )
+
     def test_unknwon_rnb_id(self):
         """
         Test that the history endpoint returns a 404 when the RNB ID does not exist
