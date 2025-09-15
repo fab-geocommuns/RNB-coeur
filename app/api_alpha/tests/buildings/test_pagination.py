75eb87447dff2c00f45657ccc88d59e08c45cbd5
Paul Etienney <paul@donatello.dev>
2025-09-15 16:02:01 +0200

    Update app/api_alpha/tests/buildings/test_pagination.py
    Co-authored-by: Léo Goffic <leo-paul.goffic@beta.gouv.fr>

--- app/api_alpha/tests/buildings/test_pagination.py
+++ app/api_alpha/tests/buildings/test_pagination.py
@@ -129,7 +129,7 @@ class EndpointTest(APITestCase):
 
         self.assertEqual(len(data["features"]), 20)
 
-        # Since the data as no expliciti ORDER BY we want to check the results have a consistent order
+        # Since the data as no explicit ORDER BY we want to check the results have a consistent order
         self.assertEqual(data["features"][0]["properties"]["rnb_id"], "000000000080")
         self.assertEqual(data["features"][5]["properties"]["rnb_id"], "000000000085")
         self.assertEqual(data["features"][16]["properties"]["rnb_id"], "000000000096")
