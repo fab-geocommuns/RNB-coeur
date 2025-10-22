13bb9918af125ad2cb172d6ce40c30425177fbde
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-10-22 14:20:26 +0200

    Fix initial attribution

--- app/batid/services/data_fix/fix_initial_addresses_attribution.py
+++ app/batid/services/data_fix/fix_initial_addresses_attribution.py
@@ -1,141 +1,189 @@
 from django.db import transaction, connection
-from batid.models import BuildingHistoryOnly, BuildingWithHistory, BuildingImport
+from batid.models import (
+    BuildingHistoryOnly,
+    BuildingWithHistory,
+    BuildingImport,
+    Building,
+)
 from typing import TypedDict
 from datetime import datetime
-from typing import Iterable
+from typing import Collection
+from typing import Union
+import math
+import time
+
+Numeric = Union[int, float]
 
 
-class BuildingHistoryItemPair(TypedDict):
-    rnb_id: str
-    bdtopo_history_or_building_item: BuildingWithHistory
-    bdnb_history_item: BuildingHistoryOnly
+def impacted_buildings_query(
+    correct_addresses_historisation_date: datetime,
+    from_rnb_id: str,
+    to_rnb_id: str,
+    limit: int,
+) -> str:
+    return f"""
+with
+impacted_bdnb_import_ids as (
+select
+	id
+from
+	batid_buildingimport as bi
+where
+	bi.import_source ilike 'bdnb%'
+	and bi.created_at < '{correct_addresses_historisation_date.isoformat()}'
+),
+all_bdtopo_import_ids as (
+select
+	id
+from
+	batid_buildingimport as bi
+where
+	bi.import_source ilike 'bdtopo%'
+),
+potential_bdnb_history_items as (
+select
+	*
+from
+	batid_building_history
+where
+	event_origin->>'source' = 'import'
+	and event_origin->>'id' in (
+	select
+		id ::text
+	from
+		impacted_bdnb_import_ids)
+	and (addresses_id is null
+		or cardinality(addresses_id) = 0)
+),
+bdtopo_hitems_with_addresses as (
+select
+	*
+from
+	batid_building_with_history
+where
+	event_origin->>'source' = 'import'
+	and event_origin->>'id' in (
+	select
+		id ::text
+	from
+		all_bdtopo_import_ids)
+	and cardinality(addresses_id) > 0
+),
+problematic_bdnb_items as (
+select
+	bdnb_hi.*,
+	bdtopo_hi.bh_id as bdtopo_bh_id
+from
+	potential_bdnb_history_items as bdnb_hi
+join bdtopo_hitems_with_addresses as bdtopo_hi
+on
+	bdnb_hi.rnb_id = bdtopo_hi.rnb_id
+	and lower(bdtopo_hi.sys_period) = upper(bdnb_hi.sys_period)
+)
+select
+	bdtopo_bh_id, rnb_id
+from
+	problematic_bdnb_items
+where rnb_id > '{from_rnb_id}' and rnb_id <= '{to_rnb_id}'
+limit {limit};
+    """
 
 
 class InitialAddressesAttributionDataFix:
-    def __init__(self, batch_size: int = 10000, start_id: int = 0) -> None:
+    def __init__(
+        self,
+        correct_addresses_historisation_date: datetime = datetime(2024, 5, 1),
+        from_rnb_id: str = "0",
+        to_rnb_id: str = "ZZZZZZZZZZZZ",
+        batch_size: int = 10000,
+    ):
+        self.correct_addresses_historisation_date = correct_addresses_historisation_date
+        self.from_rnb_id = from_rnb_id
+        self.to_rnb_id = to_rnb_id
         self.batch_size = batch_size
-        self.start_id = start_id
-        self.correct_addresses_historisation_date = datetime(2024, 5, 1)
 
-    def fix_all(self) -> None:
-        current_id = self.start_id
-        affected_bdnb_import_ids = self._get_affected_bdnb_import_ids()
-        affected_bdtopo_import_ids = self._get_affected_bdtopo_import_ids()
+    def fix_all(self):
         while True:
-            updated_row_count = self.fix_batch_initial_addresses_attribution(
-                current_id, affected_bdnb_import_ids, affected_bdtopo_import_ids
+            time_start = time.time()
+            bdtopo_history_item_ids = self._query_impacted_bdtopo_history_item_ids(
+                limit=self.batch_size
             )
-            if updated_row_count == 0:
+            if len(bdtopo_history_item_ids) == 0:
                 break
-            current_id += updated_row_count
-            print(f"Updated {updated_row_count} rows")
+            for bdtopo_history_item_id, rnb_id in bdtopo_history_item_ids:
+                self.fix_building_with_history(bdtopo_history_item_id, rnb_id)
+            time_end = time.time()
+            print(f"Time taken: {time_end - time_start} seconds", flush=True)
 
-    def fix_batch_initial_addresses_attribution(
-        self,
-        current_id: int,
-        affected_bdnb_import_ids: list[int],
-        affected_bdtopo_import_ids: list[int],
-    ) -> int:
-        updated_row_count = 0
-
-        with transaction.atomic():
-            building_history_item_pairs = self._get_building_history_item_pairs_with_missing_initial_address_attribution(
-                current_id, affected_bdnb_import_ids, affected_bdtopo_import_ids
-            )
-            for building_history_item_pair in building_history_item_pairs:
-                if self._should_fix_single_initial_addresses_attribution(
-                    building_history_item_pair
-                ):
-                    self._fix_single_initial_addresses_attribution(
-                        building_history_item_pair
-                    )
-                    updated_row_count += 1
-        return updated_row_count
-
-    def _get_affected_bdnb_import_ids(self) -> list[int]:
-        return BuildingImport.objects.filter(
-            import_source__startswith="bdnb_",
-            created_at__lt=self.correct_addresses_historisation_date,
-        ).values_list("id", flat=True)
-
-    def _get_affected_bdtopo_import_ids(self) -> list[int]:
-        return BuildingImport.objects.filter(
-            import_source__startswith="bdtopo_",
-            created_at__lt=self.correct_addresses_historisation_date,
-        ).values_list("id", flat=True)
-
-    def _should_fix_single_initial_addresses_attribution(
-        self,
-        building_history_item_pair: BuildingHistoryItemPair,
-    ) -> bool:
-        bdtopo_history_or_building_item = building_history_item_pair[
-            "bdtopo_history_or_building_item"
-        ]
-        bdnb_history_item = building_history_item_pair["bdnb_history_item"]
-        return (
-            bdnb_history_item.addresses_id is None
-            and bdtopo_history_or_building_item.addresses_id is not None
-            and bdtopo_history_or_building_item.addresses_id != []
-        )
-
-    def _fix_single_initial_addresses_attribution(
-        self,
-        building_history_item_pair: BuildingHistoryItemPair,
-    ) -> None:
-        bdtopo_history_or_building_item = building_history_item_pair[
-            "bdtopo_history_or_building_item"
-        ]
-        bdnb_history_item = building_history_item_pair["bdnb_history_item"]
+    def _query_impacted_bdtopo_history_item_ids(
+        self, limit: int
+    ) -> list[tuple[int, str]]:
         with connection.cursor() as cursor:
-            sql = "UPDATE batid_buildinghistoryonly SET addresses_id = %s WHERE bh_id = %s"
-            args = (
-                bdtopo_history_or_building_item.addresses_id,
-                bdnb_history_item.bh_id,
+            raw_sql = impacted_buildings_query(
+                self.correct_addresses_historisation_date,
+                self.from_rnb_id,
+                self.to_rnb_id,
+                limit,
             )
-            print(sql % args)
-            cursor.execute(
-                sql,
-                args,
+            cursor.execute(raw_sql)
+            return [(row[0], row[1]) for row in cursor.fetchall()]
+
+    def fix_building_with_history(
+        self, bdtopo_history_item_id: int | None, rnb_id: str
+    ):
+        previous_item_sys_period_start = None
+        addresses_id = None
+        if bdtopo_history_item_id is None:
+            building = Building.objects.get(rnb_id=rnb_id)
+            addresses_id = building.addresses_id
+            previous_item_sys_period_start = building.sys_period.lower
+        else:
+            building_history_item = BuildingHistoryOnly.objects.get(
+                bh_id=bdtopo_history_item_id
+            )
+            addresses_id = building_history_item.addresses_id
+            previous_item_sys_period_start = building_history_item.sys_period.lower
+
+        if addresses_id is None:
+            raise Exception(
+                f"Building {rnb_id} has no addresses_id on its history item"
             )
 
-    def _get_building_history_item_pairs_with_missing_initial_address_attribution(
-        self,
-        current_id: int,
-        affected_bdnb_import_ids: list[int],
-        affected_bdtopo_import_ids: list[int],
-    ) -> list[BuildingHistoryItemPair]:
-        result: list[BuildingHistoryItemPair] = []
+        while True:
+            previous_history_item = BuildingHistoryOnly.objects.filter(
+                rnb_id=rnb_id,
+                sys_period__endswith=previous_item_sys_period_start,
+            ).first()
+            if previous_history_item is None:
+                break
+            previous_item_sys_period_start = previous_history_item.sys_period.lower
+            previous_item_bh_id = previous_history_item.bh_id
+            if previous_history_item.addresses_id is not None:
+                raise Exception(
+                    f"Previous history item {previous_history_item.id} has addresses_id"
+                )
+            if previous_history_item.event_origin["source"] != "import":
+                raise Exception(
+                    f"Previous history item {previous_history_item.id} is not an import"
+                )
+            origin_import_id = previous_history_item.event_origin["id"]
+            if origin_import_id not in self._impacted_bdnb_import_ids():
+                raise Exception(
+                    f"Previous history item {previous_history_item.id} is not an impacted import"
+                )
+            sql = "UPDATE batid_building_history SET addresses_id = %s WHERE bh_id = %s"
+            with connection.cursor() as cursor:
+                # print(sql, addresses_id, previous_item_bh_id)
 
-        candidate_bdnb_history_items = BuildingHistoryOnly.objects.filter(
-            event_origin__source="import",
-            event_origin__id__in=affected_bdnb_import_ids,
-            bh_id__gt=current_id,
-            addresses_id__isnull=True,
-        )
-
-        bdtopo_history_or_building_items = BuildingWithHistory.objects.filter(
-            event_origin__source="import",
-            event_origin__id__in=affected_bdtopo_import_ids,
-            rnb_id__in=candidate_bdnb_history_items.values_list("rnb_id", flat=True),
-            addresses_id__isnull=False,
-        )
-
-        for candidate_bdnb_history_item in candidate_bdnb_history_items:
-            bdtopo_history_or_building_item = next(
-                (
-                    item
-                    for item in bdtopo_history_or_building_items
-                    if item.rnb_id == candidate_bdnb_history_item.rnb_id
-                ),
-                None,
-            )
-            if bdtopo_history_or_building_item:
-                result.append(
-                    BuildingHistoryItemPair(
-                        rnb_id=candidate_bdnb_history_item.rnb_id,
-                        bdtopo_history_or_building_item=bdtopo_history_or_building_item,
-                        bdnb_history_item=candidate_bdnb_history_item,
-                    )
+                cursor.execute(
+                    sql,
+                    (addresses_id, previous_item_bh_id),
                 )
 
-        return result
+    def _impacted_bdnb_import_ids(self) -> list[int]:
+        if not hasattr(self, "__impacted_bdnb_import_ids"):
+            self.__impacted_bdnb_import_ids = BuildingImport.objects.filter(
+                import_source__icontains="bdnb",
+                created_at__lt=self.correct_addresses_historisation_date,
+            ).values_list("id", flat=True)
+        return self.__impacted_bdnb_import_ids
--- app/batid/tests/data_fix/test_fix_initial_addresses_attribution.py
+++ app/batid/tests/data_fix/test_fix_initial_addresses_attribution.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timezone
 from django.test import TestCase
+from datetime import timedelta
 from batid.models import Address
 from batid.models import Building
 from batid.models import BuildingImport
 from batid.models import BuildingHistoryOnly
+from batid.models import BuildingWithHistory
 from batid.services.data_fix.fix_initial_addresses_attribution import (
     InitialAddressesAttributionDataFix,
 )
@@ -14,6 +16,16 @@ def now():
 
 
 class TestFixInitialAddressesAttribution(TestCase):
+    def _create_import(
+        self, import_source: str, created_at: datetime
+    ) -> BuildingImport:
+        new_import = BuildingImport.objects.create(
+            import_source=import_source,
+        )
+        new_import.created_at = created_at
+        BuildingImport.objects.bulk_update([new_import], ["created_at"])
+        return new_import
+
     def _create_building(self, rnb_id: str, event_origin: dict) -> None:
         Building.objects.create(
             rnb_id=rnb_id,
@@ -29,60 +41,210 @@ class TestFixInitialAddressesAttribution(TestCase):
         )
 
     def setUp(self):
-        address1 = Address.objects.create(id="cle_interop_1")
-        address2 = Address.objects.create(id="cle_interop_2")
-        bdnb_import = BuildingImport.objects.create(
-            import_source="bdnb_test",
-            created_at=now(),
+        three_days_ago = now() - timedelta(days=3)
+        two_days_ago = now() - timedelta(days=2)
+        correct_addresses_historisation_date = two_days_ago
+
+        self.address1 = Address.objects.create(id="cle_interop_1")
+        self.address2 = Address.objects.create(id="cle_interop_2")
+
+        self.impacted_bdnb_import = self._create_import("bdnb_test", three_days_ago)
+        self.impacted_bdtopo_import = self._create_import("bdtopo_test", three_days_ago)
+
+        self.fixer = InitialAddressesAttributionDataFix(
+            correct_addresses_historisation_date=correct_addresses_historisation_date,
         )
-        bdtopo_import = BuildingImport.objects.create(
-            import_source="bdtopo_test",
-            created_at=now(),
-        )
+
+    def test_ignores_on_building_with_other_history_items_between_bdnb_and_bdtopo(self):
         self._create_building(
-            "need_fix", event_origin={"source": "import", "id": bdnb_import.id}
+            "problematic",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
         )
         self._update_building(
-            "need_fix",
+            "problematic",
+            event_origin={"source": "contribution", "id": 1},
+            addresses_id=[self.address1.id],
+        )
+        self._update_building(
+            "problematic",
+            event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            addresses_id=[self.address2.id],
+        )
+        self.fixer.fix_all()
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(
+                rnb_id="problematic",
+                event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+            ).addresses_id,
+            None,
+        )
+        self.assertEqual(
+            BuildingWithHistory.objects.get(
+                rnb_id="problematic", event_origin={"source": "contribution", "id": 1}
+            ).addresses_id,
+            [self.address1.id],
+        )
+        self.assertEqual(
+            BuildingWithHistory.objects.get(
+                rnb_id="problematic",
+                event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            ).addresses_id,
+            [self.address2.id],
+        )
+
+    def test_fixes_single_initial_addresses_attribution_in_simple_case(self):
+        # Problematic case with no subsequent update
+        self._create_building(
+            "need_fix1",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+        )
+        self._update_building(
+            "need_fix1",
             event_origin={
                 "source": "import",
-                "id": bdtopo_import.id,
+                "id": self.impacted_bdtopo_import.id,
             },
-            addresses_id=[address1.id, address2.id],
+            addresses_id=[self.address1.id, self.address2.id],
         )
+        self.fixer.fix_all()
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(rnb_id="need_fix1").addresses_id,
+            [self.address1.id, self.address2.id],
+        )
+        self.assertEqual(
+            BuildingWithHistory.objects.get(
+                rnb_id="need_fix1",
+                event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            ).addresses_id,
+            [self.address1.id, self.address2.id],
+        )
+
+    def test_fixes_single_initial_addresses_attribution_in_case_with_subsequent_updates(
+        self,
+    ):
         self._create_building(
-            "ok_noaddress", event_origin={"source": "import", "id": bdnb_import.id}
+            "need_fix2",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+        )
+        self._update_building(
+            "need_fix2",
+            event_origin={
+                "source": "import",
+                "id": self.impacted_bdtopo_import.id,
+            },
+            addresses_id=[self.address1.id, self.address2.id],
+        )
+        self._update_building(
+            "need_fix2",
+            event_origin={
+                "source": "contribution",
+                "id": 1,
+            },
+            addresses_id=[self.address2.id],
+        )
+        self.fixer.fix_all()
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(
+                rnb_id="need_fix2",
+                event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+            ).addresses_id,
+            [self.address1.id, self.address2.id],
+        )
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(
+                rnb_id="need_fix2",
+                event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            ).addresses_id,
+            [self.address1.id, self.address2.id],
+        )
+        self.assertEqual(
+            BuildingWithHistory.objects.get(
+                rnb_id="need_fix2",
+                event_origin={"source": "contribution", "id": 1},
+            ).addresses_id,
+            [self.address2.id],
+        )
+
+    def test_fixes_initial_addresses_attribution_when_more_than_one_bdnb_update(self):
+        self._create_building(
+            "need_fix3",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+        )
+        self._update_building(
+            "need_fix3",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
+            addresses_id=[],
+        )
+        self._update_building(
+            "need_fix3",
+            event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            addresses_id=[self.address1.id, self.address2.id],
+        )
+
+    def test_does_not_change_building_history_without_addresses(self):
+        # Not problematic case: building without addresses
+        self._create_building(
+            "ok_noaddress",
+            event_origin={"source": "import", "id": self.impacted_bdnb_import.id},
         )
         self._update_building(
             "ok_noaddress",
-            event_origin={"source": "import", "id": bdtopo_import.id},
-            addresses_id=[],
+            event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            addresses_id=None,
         )
+        self.fixer.fix_all()
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(rnb_id="ok_noaddress").addresses_id,
+            None,
+        )
+        self.assertEqual(
+            Building.objects.get(rnb_id="ok_noaddress").addresses_id,
+            None,
+        )
+
+    def test_does_not_change_building_created_manually(self):
+        # Not problematic case: building created manually
         self._create_building(
             "ok_manual", event_origin={"source": "contribution", "id": 1}
         )
         self._update_building(
             "ok_manual",
-            event_origin={"source": "import", "id": bdtopo_import.id},
-            addresses_id=[address1.id],
+            event_origin={"source": "import", "id": self.impacted_bdtopo_import.id},
+            addresses_id=[self.address1.id],
+        )
+        self.fixer.fix_all()
+        self.assertEqual(
+            BuildingHistoryOnly.objects.get(rnb_id="ok_manual").addresses_id,
+            None,
+        )
+        self.assertEqual(
+            Building.objects.get(rnb_id="ok_manual").addresses_id,
+            [self.address1.id],
         )
 
-    def test_fixes_single_initial_addresses_attribution(self):
-        fixer = InitialAddressesAttributionDataFix()
-        fixer.fix_all()
-        self.assertEqual(
-            BuildingHistoryOnly.objects.get(rnb_id="needsfix").addresses_id,
-            [address1.id, address2.id],
+    def test_does_not_change_building_created_by_non_impacted_imports(self):
+        after_correct_addresses_historisation_date = now()
+        unimpacted_bdnb_import = self._create_import(
+            "bdnb_test", after_correct_addresses_historisation_date
         )
-
-    def test_does_not_change_building_history_without_addresses(self):
-        fixer = InitialAddressesAttributionDataFix()
-        fixer.fix_all()
+        unimpacted_bdtopo_import = self._create_import(
+            "bdtopo_test", after_correct_addresses_historisation_date
+        )
+        self._create_building(
+            "ok_noimpact",
+            event_origin={"source": "import", "id": unimpacted_bdnb_import.id},
+        )
+        self._update_building(
+            "ok_noimpact",
+            event_origin={"source": "import", "id": unimpacted_bdtopo_import.id},
+            addresses_id=[self.address1.id],
+        )
+        self.fixer.fix_all()
         self.assertEqual(
-            BuildingHistoryOnly.objects.get(rnb_id="oknoaddress1").addresses_id,
-            [],
+            BuildingHistoryOnly.objects.get(rnb_id="ok_noimpact").addresses_id,
+            None,
         )
         self.assertEqual(
-            Building.objects.get(rnb_id="oknoaddress1").addresses_id,
-            [],
+            Building.objects.get(rnb_id="ok_noimpact").addresses_id,
+            [self.address1.id],
         )
