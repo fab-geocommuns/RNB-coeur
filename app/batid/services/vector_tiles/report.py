fdbf70642a2deb6f7434be486ef2873915799e02
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-11-18 17:17:00 +0100

    Return tags in report tiles

--- app/batid/services/vector_tiles/report.py
+++ app/batid/services/vector_tiles/report.py
@@ -8,14 +8,8 @@ from batid.services.vector_tiles.common import TileParams
 def envelope_to_report_sql(env: Envelope) -> str:
 
     params = {
-        "table": Report._meta.db_table,
-        "srid": str(4326),
-        "attrColumns": "id, status",
-        "geomColumn": "point",
+        "env": envelope_to_bounds_sql(env),
     }
-
-    tbl = params.copy()
-    tbl["env"] = envelope_to_bounds_sql(env)
 
     sql_tmpl = """
             WITH
@@ -24,14 +18,24 @@ def envelope_to_report_sql(env: Envelope) -> str:
                        {env}::box2d AS b2d
             ),
             mvtgeom AS (
-                SELECT ST_AsMVTGeom(ST_Transform(t.{geomColumn}, 3857), bounds.b2d) AS geom,
-                       {attrColumns}
-                FROM {table} t, bounds
-                WHERE ST_Intersects(t.{geomColumn}, ST_Transform(bounds.geom, {srid}))
+                SELECT ST_AsMVTGeom(ST_Transform(report.point, 3857), bounds.b2d) AS geom,
+                       report.id, report.status, ARRAY_AGG(tag.id) AS tag_ids
+                FROM bounds, batid_report AS report 
+                LEFT JOIN taggit_taggeditem AS tagged_item
+                    ON tagged_item.object_id = report.id
+                LEFT JOIN django_content_type
+                    ON tagged_item.content_type_id = django_content_type.id
+                    AND django_content_type.model = 'report'
+                    AND django_content_type.app_label = 'batid'
+                LEFT JOIN taggit_tag AS tag
+                    ON tagged_item.tag_id = tag.id
+                WHERE ST_Intersects(report.point, ST_Transform(bounds.geom, 4326))
+                GROUP BY ST_AsMVTGeom(ST_Transform(report.point, 3857), bounds.b2d),
+                       report.id, report.status
             )
             SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom
         """
-    return sql_tmpl.format(**tbl)
+    return sql_tmpl.format(**params)
 
 
 def reports_tiles_sql(tile: TileParams) -> str:
