1b098292b02cfdfeefc9161341d81d717644ef51
Léo Goffic <leo-paul.goffic@beta.gouv.fr>
2025-11-25 14:35:15 +0100

    Fix shape non retournée dans le endpoint de parcelle (#776)
    * Move plots test in own file
    * Extract BuildingPlotView
    * Add shape to BuildingPlotSerializer

--- app/api_alpha/urls.py
+++ app/api_alpha/urls.py
@@ -4,6 +4,7 @@ from django.urls import re_path
 from rest_framework import routers
 
 from api_alpha.endpoints.buildings.list_create_buildings import ListCreateBuildings
+from api_alpha.endpoints.buildings.plot import BuildingPlotView
 from api_alpha.endpoints.buildings.single_building import SingleBuilding
 from api_alpha.endpoints.buildings.single_building import SingleBuildingHistory
 from api_alpha.endpoints.ogc.views import OGCBuildingItemsView
@@ -28,7 +29,6 @@ from api_alpha.views import ADSViewSet
 from api_alpha.views import BuildingAddressView
 from api_alpha.views import BuildingClosestView
 from api_alpha.views import BuildingGuessView
-from api_alpha.views import BuildingPlotView
 from api_alpha.views import ChangePassword
 from api_alpha.views import CreateUserView
 from api_alpha.views import DiffusionDatabaseView
