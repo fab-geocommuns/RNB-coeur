d94c90dc0fcee03aaa0fb64a0374b5111ecbc4e3
Léo Goffic <leo-paul.goffic@beta.gouv.fr>
2025-12-02 15:55:09 +0100

    Rendre explicite les utilisateurs sans mot-de-passe (#748)
    * Set explicitely unusable passwords

---
+++ app/api_alpha/endpoints/ads/create_token.py
@@ -0,0 +1,70 @@
+import json
+from typing import Any
+
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.http import HttpResponse
+from django.http import JsonResponse
+from drf_spectacular.utils import extend_schema
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from batid.models import Organization
+from batid.utils.constants import ADS_GROUP_NAME
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
+
+@extend_schema(exclude=True)
+class CreateAdsTokenView(APIView):
+    permission_classes = [IsSuperUser]
+
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+        try:
+            with transaction.atomic():
+                json_users = json.loads(request.body)
+                users = []
+
+                for json_user in json_users:
+                    user, created = User.objects.get_or_create(
+                        username=json_user["username"],
+                        defaults={
+                            "email": json_user.get("email", None),
+                        },
+                    )
+
+                    group, created = Group.objects.get_or_create(name=ADS_GROUP_NAME)
+                    user.groups.add(group)
+                    user.set_unusable_password()
+                    user.save()
+
+                    organization, created = Organization.objects.get_or_create(
+                        name=json_user["organization_name"],
+                        defaults={
+                            "managed_cities": json_user["organization_managed_cities"]
+                        },
+                    )
+
+                    organization.users.add(user)
+                    organization.save()
+
+                    token, created = Token.objects.get_or_create(user=user)
+
+                    users.append(
+                        {
+                            "username": user.username,
+                            "organization_name": json_user["organization_name"],
+                            "email": user.email,
+                            "token": token.key,
+                        }
+                    )
+
+                return JsonResponse({"created_users": users})
+        except json.JSONDecodeError:
+            return HttpResponse("Invalid JSON", status=400)
