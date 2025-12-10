564cb25ca6d9800f0a8579072b00182485c4e57d
Léo-Paul Goffic <leo.goffic@gmail.com>
2025-12-10 15:22:26 +0100

    Revert "Ajout d'un captcha (#769)"
    This reverts commit e4ed50f9a355b584302ec8ed051ba72826bb78f1.

--- app/api_alpha/urls.py
+++ app/api_alpha/urls.py
@@ -4,7 +4,6 @@ from django.urls import re_path
 from rest_framework import routers
 
 from api_alpha.endpoints.ads.create_token import CreateAdsTokenView
-from api_alpha.endpoints.auth.create_user import CreateUserView
 from api_alpha.endpoints.buildings.get_diff import DiffView
 from api_alpha.endpoints.buildings.list_create_buildings import ListCreateBuildings
 from api_alpha.endpoints.buildings.plot import BuildingPlotView
@@ -32,6 +31,7 @@ from api_alpha.views import BuildingAddressView
 from api_alpha.views import BuildingClosestView
 from api_alpha.views import BuildingGuessView
 from api_alpha.views import ChangePassword
+from api_alpha.views import CreateUserView
 from api_alpha.views import DiffusionDatabaseView
 from api_alpha.views import get_all_endpoints_schema
 from api_alpha.views import get_stats
