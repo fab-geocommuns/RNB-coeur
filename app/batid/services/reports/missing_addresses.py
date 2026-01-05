ac5b01aa9ea440a7462499e49464d4dd11aa24e0
Francis Chabouis <fchabouis@gmail.com>
2026-01-05 15:50:05 +0100

    create reports for department

--- app/batid/services/reports/missing_addresses.py
+++ app/batid/services/reports/missing_addresses.py
@@ -11,7 +11,6 @@ from batid.services.RNB_team_user import get_RNB_team_user
 
 
 def generate_missing_addresses_reports(reports_number, insee_code=None):
-    team_rnb = get_RNB_team_user()
 
     raw_sql = """
 select
@@ -38,21 +37,61 @@ limit %s;
         rnb_ids = cursor.fetchall()
 
         with transaction.atomic():
-            creation_uuid = uuid.uuid4()
-            for rnb_id in rnb_ids:
-                rnb_id = rnb_id[0]
-                building = Building.objects.get(rnb_id=rnb_id)
+            create_reports(rnb_ids, ["Bâtiment sans adresse"])
 
-                Report.create(
-                    point=building.point,  # type: ignore
-                    building=building,
-                    text=f"Ce bâtiment d'une surface supérieure à 100m² n'a pas d'adresse associée.",
-                    email=None,
-                    user=team_rnb,
-                    tags=["Bâtiment sans adresse"],
-                    creation_batch_uuid=creation_uuid,
-                )
+        logging.info(
+            f"{len(rnb_ids)} signalements ont été créés pour des bâtiments de plus de 100m² sans adresse situé sur la commune ayant pour code insee {insee_code}."
+        )
 
-            logging.info(
-                f"{len(rnb_ids)} signalements ont été créés pour des bâtiments de plus de 100m² sans adresse situé sur la commune ayant pour code insee {insee_code}."
-            )
+
+def generate_missing_addresses_reports_dep(reports_number, dep_code):
+
+    raw_sql = """
+select
+	bb.rnb_id
+from
+	batid_building bb
+left join batid_report br on
+	br.building_id = bb.id
+inner join batid_department_subdivided d on
+	ST_INTERSECTS(d.shape, bb.shape) and d.code = %s
+where
+	st_area(bb.shape::geography) > 100
+	and bb.addresses_id = '{}'
+	and br.building_id is null
+    and bb.is_active
+    and (bb.status = ANY(%s))
+limit %s;
+    """
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            raw_sql, [dep_code, BuildingStatus.REAL_BUILDINGS_STATUS, reports_number]
+        )
+        rnb_ids = cursor.fetchall()
+
+        with transaction.atomic():
+            create_reports(rnb_ids, ["Bâtiment sans adresse", "La galette du RNB"])
+
+        logging.info(
+            f"{len(rnb_ids)} signalements ont été créés pour des bâtiments de plus de 100m² sans adresse situé dans le département {dep_code}."
+        )
+
+
+def create_reports(rnb_ids, tags):
+    creation_uuid = uuid.uuid4()
+    team_rnb = get_RNB_team_user()
+
+    for rnb_id in rnb_ids:
+        rnb_id = rnb_id[0]
+        building = Building.objects.get(rnb_id=rnb_id)
+
+        Report.create(
+            point=building.point,  # type: ignore
+            building=building,
+            text=f"Ce bâtiment d'une surface supérieure à 100m² n'a pas d'adresse associée.",
+            email=None,
+            user=team_rnb,
+            tags=tags,
+            creation_batch_uuid=creation_uuid,
+        )
