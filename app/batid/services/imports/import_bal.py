1402a614b32f101ce11351d78dafb5a1f3b8264e
pauletienney <paul@donatello.dev>
2026-02-04 16:15:30 +0100

    faster query

--- app/batid/services/imports/import_bal.py
+++ app/batid/services/imports/import_bal.py
@@ -16,6 +16,8 @@ from batid.models import BuildingImport
 from batid.services.bdg_status import BuildingStatus
 from batid.services.imports import building_import_history
 from batid.services.source import Source
+from django.db import connection
+from batid.utils.db import dictfetchall
 
 
 def create_all_bal_links_tasks(dpts: list):
@@ -96,34 +98,35 @@ def create_dpt_bal_rnb_links(src_params: dict, bulk_launch_uuid=None):
 
 def find_bdg_to_link(address_point: Point, cle_interop: str) -> Optional[Building]:
 
-    bdg = _match_bdg_intersecting(address_point)
+    # Simple Intersects approach first
+    matching_rnb_id = _match_bdg_intersecting(address_point)
 
-    if bdg is None:
+    if matching_rnb_id is None:
 
-        bdg = _match_bdg_on_plot(address_point)
+        # There was no match
+        # We try the more complex plot-based approach
 
-    if not isinstance(bdg, Building):
+        matching_rnb_id = _match_bdg_on_plot(address_point)
+
+    if matching_rnb_id is None:
+
+        # Still no match
+        # We give up
+
         return None
 
     # We do NOT want to create the bdg <> address link if the same link exists or has existed in the past
-    if (
-        cle_interop in bdg.current_addresses  # type: ignore[attr-defined]
-        or cle_interop in bdg.past_addresses  # type: ignore[attr-defined]
-    ):
+    if _known_building_address_link(cle_interop, matching_rnb_id):
         return None
 
-    return bdg
+    return Building.objects.get(rnb_id=matching_rnb_id)
 
 
-def _match_bdg_intersecting(address_point: Point) -> Optional[Building]:
+def _match_bdg_intersecting(address_point: Point) -> Optional[str]:
 
     on_bdg_sql = """
-        SELECT bdg.id, bdg.rnb_id, bdg.is_active, bdg.updated_at,
-        COALESCE (bdg.addresses_id, '{}') AS current_addresses,
-        COALESCE(array_agg(DISTINCT unnested_address_id), '{}') AS past_addresses
+        SELECT bdg.rnb_id
         FROM batid_building as bdg
-        LEFT JOIN batid_building_history as history on history.rnb_id = bdg.rnb_id
-        LEFT JOIN LATERAL unnest(history.addresses_id) AS unnested_address_id ON TRUE
         WHERE ST_Intersects(bdg.shape, %(address_point)s)
         AND bdg.status IN %(status)s
         AND bdg.is_active = TRUE
@@ -135,58 +138,69 @@ def _match_bdg_intersecting(address_point: Point) -> Optional[Building]:
         "status": tuple(BuildingStatus.REAL_BUILDINGS_STATUS),
     }
 
-    bdgs = Building.objects.raw(on_bdg_sql, params)
+    with connection.cursor() as cursor:
+        rows = dictfetchall(cursor, on_bdg_sql, params)
 
-    if len(bdgs) != 1:
+    if len(rows) != 1:
         return None
 
-    return bdgs[0]
+    return rows[0]["rnb_id"]
 
 
-def _match_bdg_on_plot(address_point: Point) -> Optional[Building]:
+def _match_bdg_on_plot(address_point: Point) -> Optional[str]:
 
-    # Quick note on the query below:
+    # Quick note on the building match below:
     # We want to be SUPER conservative when linking a BAL address to a BDG via the plot.
     # There are many many edge cases where this can go wrong.
     # So we add the following constraints:
-    # - The address point buffer (5m) must intersect only one plot
+    # - The address point must be within 5 meters of one and only one plot
     # - Any building with more than 50% of its area on that plot is considered as belonging to that plot
     # - The plot must have only one building matching the above condition
     # - The matching building should have 90+% of its area on that plot
     # - The building must be active and in a "real" status
 
     on_plot_sql = """
-            SELECT bdg.id, bdg.rnb_id, bdg.is_active, bdg.updated_at, 
-            CASE WHEN ST_Area(bdg.shape) = 0 THEN 1 ELSE St_Area(ST_Intersection(bdg.shape, plot.shape)) / St_Area(bdg.shape) END AS bdg_cover_ratio,
-            COALESCE (bdg.addresses_id, '{}') AS current_addresses,
-            COALESCE(array_agg(DISTINCT unnested_address_id), '{}') AS past_addresses
-            FROM batid_building as bdg
-            JOIN batid_plot as plot ON ST_Intersects(bdg.shape, plot.shape)
-            LEFT JOIN batid_building_history as history on history.rnb_id = bdg.rnb_id
-            LEFT JOIN LATERAL unnest(history.addresses_id) AS unnested_address_id ON TRUE
-            WHERE ST_Intersects(plot.shape, ST_Buffer(%(address_point)s, 5))
-            AND (CASE WHEN ST_Area(bdg.shape) = 0 THEN 1 ELSE ST_Area(ST_Intersection(bdg.shape, plot.shape)) / ST_Area(bdg.shape) END) > 0.5
-            AND bdg.status IN %(status)s
-            AND bdg.is_active = TRUE
-            GROUP BY bdg.id, bdg.rnb_id, bdg.addresses_id, bdg.is_active, bdg.updated_at, bdg.shape, plot.shape
-            HAVING COUNT(plot.id) = 1
-        """
+        select bdg.rnb_id, plot.id as plot_id,
+        CASE WHEN ST_Area(bdg.shape) = 0 THEN 1 ELSE St_Area(ST_Intersection(bdg.shape, plot.shape)) / St_Area(bdg.shape) END AS bdg_cover_ratio
+        from batid_plot as plot
+        inner join batid_building bdg on st_intersects(plot.shape, bdg.shape)
+        where st_dwithin(%(address_point)s, plot.shape::geography, 5)
+        AND bdg.status IN %(status)s
+        AND bdg.is_active = true
+    ;
+    """
 
     params = {
         "address_point": f"{address_point}",
         "status": tuple(BuildingStatus.REAL_BUILDINGS_STATUS),
     }
 
-    bdgs = Building.objects.raw(on_plot_sql, params)
+    with connection.cursor() as cursor:
+        results = dictfetchall(cursor, on_plot_sql, params)
 
-    if len(bdgs) != 1:
+    # First we verify there is only one plot in the results
+    plot_ids = set()
+    for row in results:
+        plot_ids.add(row["plot_id"])
+
+    if len(plot_ids) != 1:
         return None
 
-    # If the building does not have 90%+ of its area on that plot, we refuse to link it
-    if bdgs[0].bdg_cover_ratio < 0.9:  # type: ignore[attr-defined]
+    # Then we verify there is only one building with more than 50% area on that plot
+    candidate_bdgs = []
+
+    for row in results:
+        if row["bdg_cover_ratio"] >= 0.5:
+            candidate_bdgs.append(row)
+
+    if len(candidate_bdgs) != 1:
         return None
 
-    return bdgs[0]
+    # Finally we verify the matching building has 90%+ of its area on that plot
+    if candidate_bdgs[0]["bdg_cover_ratio"] < 0.9:
+        return None
+
+    return candidate_bdgs[0]["rnb_id"]
 
 
 def find_and_update_bdg(  # type: ignore[return]
@@ -237,3 +251,23 @@ def process_batch(batch: list, bdg_import: BuildingImport):
         bdg_import.building_refused_count += refused_count  # type: ignore
         bdg_import.building_updated_count += updated_count  # type: ignore
         bdg_import.save()
+
+
+def _known_building_address_link(cle_interop: str, rnb_id: str) -> bool:
+
+    q = """
+        select 1
+        from batid_building_with_history as bdg
+        where bdg.rnb_id = %(rnb_id)s
+        and %(cle_interop)s = any(bdg.addresses_id)
+    """
+
+    params = {
+        "rnb_id": rnb_id,
+        "cle_interop": cle_interop,
+    }
+
+    with connection.cursor() as cursor:
+        rows = dictfetchall(cursor, q, params)
+
+    return len(rows) > 0
