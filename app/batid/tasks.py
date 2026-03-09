3feadaa22dd870698b024ef1e6b51fbb59cc88b1
Francis Chabouis <fchabouis@gmail.com>
2026-03-09 16:29:16 +0100

    reduce batch size

--- app/batid/tasks.py
+++ app/batid/tasks.py
@@ -510,7 +510,7 @@ def queue_ban_address_text_update(
     return f"Queued text update for {len(dpts)} departments"
 
 
-def delete_unlinked_obsolete_addresses_task(batch_size: int = 10000):
+def delete_unlinked_obsolete_addresses_task(batch_size: int = 1000):
     from batid.services.imports.update_addresses_ban import (
         delete_unlinked_obsolete_addresses,
     )
