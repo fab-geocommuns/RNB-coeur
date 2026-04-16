4dc42a543bbc84543073295cf3767155a6678ed8
Paul Etienney <paul@donatello.dev>
2026-04-16 10:41:59 +0200

    Update app/api_alpha/endpoints/auth/pro_connect.py
    Co-authored-by: Francis Chabouis <fchabouis@gmail.com>

--- app/api_alpha/endpoints/auth/pro_connect.py
+++ app/api_alpha/endpoints/auth/pro_connect.py
@@ -43,7 +43,7 @@ logger = logging.getLogger(__name__)
 #  — generates a nonce + state, builds the Pro Connect, authorization URL, and returns it to the site.
 #
 #  3. Website redirects (rnb-site)
-#  - the website receive the AuthorizeView response and use the authorizaton URL it conains to redirect the user
+#  - the website receives the AuthorizeView response and uses the authorizaton URL it contains to redirect the user
 #  to the Pro Connect site
 #
 #  4. Pro Connect (outside RNB)
