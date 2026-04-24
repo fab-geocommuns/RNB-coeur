6fc4c8ca667da67e65a293bb8beb36f3248efcdc
pauletienney <paul@donatello.dev>
2026-04-24 14:28:29 +0200

    fix conflict

--- app/batid/tests/test_rnbuser.py
+++ app/batid/tests/test_rnbuser.py
@@ -23,7 +23,6 @@ class TestRNBUser(TestCase):
         u = User.objects.get(username="johndoe")
         managed_codes = get_managed_insee_codes(u)
         managed_codes.sort()
-<<<<<<< replug_ads
         self.assertListEqual(managed_codes, ["12345", "67890"])
 
     def test_user_with_no_profile_returns_empty(self):
@@ -36,68 +35,3 @@ class TestRNBUser(TestCase):
         u = User.objects.create_user(username="noordg", email="no@test.com")
         UserProfile.objects.create(user=u)
         self.assertListEqual(get_managed_insee_codes(u), [])
-=======
-
-        expected = ["12345", "42420", "67890"]
-        expected.sort()
-
-        self.assertListEqual(managed_codes, expected)
-
-
-class TestPopulateOrganizationOnProfile(TestCase):
-    """
-    After data migration: UserProfile.organization is set from M2M.
-    - A user in exactly one org gets that org assigned.
-    - A user in multiple orgs gets one of them assigned (first by pk).
-    - A user in no org keeps organization=None.
-    """
-
-    def setUp(self):
-        self.org_a = Organization.objects.create(name="Org A", managed_cities=["11000"])
-        self.org_b = Organization.objects.create(name="Org B", managed_cities=["22000"])
-
-        self.user_one_org = User.objects.create_user(
-            username="one_org", email="one_org@test.com"
-        )
-        UserProfile.objects.create(user=self.user_one_org)
-        self.org_a.users.add(self.user_one_org)
-
-        self.user_multi_org = User.objects.create_user(
-            username="multi_org", email="multi_org@test.com"
-        )
-        UserProfile.objects.create(user=self.user_multi_org)
-        self.org_a.users.add(self.user_multi_org)
-        self.org_b.users.add(self.user_multi_org)
-
-        self.user_no_org = User.objects.create_user(
-            username="no_org", email="no_org@test.com"
-        )
-        UserProfile.objects.create(user=self.user_no_org)
-
-    def test_single_org_user_gets_org_assigned(self):
-        populate_organization_on_profiles()
-        self.user_one_org.profile.refresh_from_db()
-        self.assertEqual(self.user_one_org.profile.organization, self.org_a)
-
-    def test_multi_org_user_gets_first_org_by_pk(self):
-        populate_organization_on_profiles()
-        self.user_multi_org.profile.refresh_from_db()
-        # first org by pk
-        self.assertEqual(self.user_multi_org.profile.organization, self.org_a)
-
-    def test_no_org_user_keeps_null(self):
-        populate_organization_on_profiles()
-        self.user_no_org.profile.refresh_from_db()
-        self.assertIsNone(self.user_no_org.profile.organization)
-
-    def test_user_without_profile_gets_profile_created(self):
-        """User with an org membership but no UserProfile row: profile is created and org is assigned."""
-        user = User.objects.create_user(
-            username="no_profile", email="no_profile@test.com"
-        )
-        # No UserProfile created here
-        self.org_a.users.add(user)
-        populate_organization_on_profiles()
-        profile = UserProfile.objects.get(user=user)
-        self.assertEqual(profile.organization, self.org_a)
->>>>>>> user_org_1n
