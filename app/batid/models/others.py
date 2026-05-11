52a0eefec1514b9bb3a4cc9d9362569a70d9706e
pauletienney <paul@donatello.dev>
2026-05-11 17:31:43 +0200

    select org via name in userprofile

--- app/batid/models/others.py
+++ app/batid/models/others.py
@@ -202,6 +202,9 @@ class Organization(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
