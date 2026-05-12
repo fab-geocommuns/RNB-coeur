d7ac9d8eb197c5a21b94e78911ba484e8d7277f3
Francis Chabouis <fchabouis@gmail.com>
2026-05-12 17:04:59 +0200

    fix test and change default to list instead of []

--- app/batid/models/building.py
+++ app/batid/models/building.py
@@ -98,7 +98,7 @@ class BuildingAbstract(models.Model):
     # this field is the source of truth for the building <> address link
     # it contains BAN ids (clé d'interopérabilité)
     addresses_id = ArrayField(models.CharField(max_length=40), null=True)
-    marked_as_correct_by = ArrayField(models.IntegerField(), null=True, default=[])
+    marked_as_correct_by = ArrayField(models.IntegerField(), null=True, default=list)
 
     class Meta:
         abstract = True
